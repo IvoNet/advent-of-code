e0ad3c7d4a0c6b3b3009058b247b223363d1aac3
Ivo Woltring <ivo@ivonet.nl>
2021-12-02 17:00:07 +0100

    2020 day 5

--- 2020/day-4.py
+++ 2020/day-4.py
@@ -70,69 +70,69 @@ def part_2(data):
         valid = True
         key = "byr"
         if int(pp[key]) < 1920 or int(pp[key]) > 2002:
-            print(key, pp)
+            # print(key, pp)
             valid = False
 
         key = "iyr"
         if int(pp[key]) < 2010 or int(pp[key]) > 2020:
-            print(key, pp)
+            # print(key, pp)
             valid = False
 
         key = "eyr"
         value = int(pp[key])
         if value < 2020 or value > 2030:
-            print(key, pp)
+            # print(key, pp)
             valid = False
 
         key = "hgt"
         if pp[key].endswith("in"):
             if int(pp[key].replace("in", "")) < 59 or int(pp[key].replace("in", "")) > 79:
-                print("hgt 1", pp)
+                # print("hgt 1", pp)
                 valid = False
         elif pp[key].endswith("cm"):
             if int(pp[key].replace("cm", "")) < 150 or int(pp[key].replace("cm", "")) > 193:
-                print("hgt 2", pp)
+                # print("hgt 2", pp)
                 valid = False
         else:
-            print("hgt 3", pp)
+            # print("hgt 3", pp)
             valid = False
 
         key = "hcl"
         if not re.match("^#[0-9a-f]{6}$", pp[key]):
-            print(key, pp)
+            # print(key, pp)
             valid = False
 
         key = "ecl"
         if not pp[key] in "amb blu brn gry grn hzl oth".split(" "):
-            print(key, pp)
+            # print(key, pp)
             valid = False
 
         key = "pid"
         if not re.match("^[0-9]{9}$", pp[key]):
-            print(key, pp)
+            # print(key, pp)
             valid = False
 
         if valid:
             valid_passports += 1
-            print("valid", pp)
+            # print("valid", pp)
             parsed_valid_passports.append(pp)
     return valid_passports
 
 
 if __name__ == '__main__':
     source = read_data("day-4.txt")
-    print(part_2("""eyr:1972 cid:100
-hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
-
-iyr:2019
-hcl:#602927 eyr:1967 hgt:170cm
-ecl:grn pid:012533040 byr:1946
-
-hcl:dab227 iyr:2012
-ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
-
-hgt:59cm ecl:zzz
-eyr:2038 hcl:74454a iyr:2023
-pid:3556412378 byr:2007"""))
-    print(part_1(source)[1])  # 213
-    print(part_2(source))  # 213
+    #     print(part_2("""eyr:1972 cid:100
+    # hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+    #
+    # iyr:2019
+    # hcl:#602927 eyr:1967 hgt:170cm
+    # ecl:grn pid:012533040 byr:1946
+    #
+    # hcl:dab227 iyr:2012
+    # ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+    #
+    # hgt:59cm ecl:zzz
+    # eyr:2038 hcl:74454a iyr:2023
+    # pid:3556412378 byr:2007"""))
+    print("Part 1:", part_1(source)[1])  # 213
+    print("Part 2:", part_2(source))  # 147
--- 2020/day-5.py
+++ 2020/day-5.py
@@ -9,6 +9,33 @@ __doc__ = """
 # Part 1
 ##############################################################################
 
+For example, consider just the first seven characters of FBFBBFFRLR:
+
+Start by considering the whole range, rows 0 through 127.
+F means to take the lower half, keeping rows 0 through 63.
+B means to take the upper half, keeping rows 32 through 63.
+F means to take the lower half, keeping rows 32 through 47.
+B means to take the upper half, keeping rows 40 through 47.
+B keeps rows 44 through 47.
+F keeps rows 44 through 45.
+The final F keeps the lower of the two, row 44.
+
+The last three characters will be either L or R; these specify exactly one 
+of the 8 columns of seats on the plane (numbered 0 through 7). The same 
+process as above proceeds again, this time with only three steps. L means 
+to keep the lower half, while R means to keep the upper half.
+
+For example, consider just the last 3 characters of FBFBBFFRLR:
+
+Start by considering the whole range, columns 0 through 7.
+R means to take the upper half, keeping columns 4 through 7.
+L means to take the lower half, keeping columns 4 through 5.
+The final R keeps the upper of the two, column 5.
+
+So, decoding FBFBBFFRLR reveals that it is the seat at row 44, column 5.
+
+Every seat also has a unique seat ID: multiply the row by 8, then add the 
+column. In this example, the seat has ID 44 * 8 + 5 = 357.
 ##############################################################################
 # Part 2
 ##############################################################################
@@ -17,16 +44,61 @@ __doc__ = """
 
 from ivonet import get_data
 
+ROWS = list(range(128))
+COLLUMS = list(range(8))
+
+
+def split_middle(rows):
+    middle_index = len(rows) // 2
+    left = rows[:middle_index]
+    right = rows[middle_index:]
+    return left, right
+
 
 def part_1(data):
-    pass
+    seats = []
+    highest_seat = 0
+    # data = ["BFFFBBFRRR", "FFFBBBFRRR", "BBFFBBFRLL"]
+    for seat in data:
+        rows = ROWS.copy()
+        for row in seat[:7]:
+            front, back = split_middle(rows)
+            if row == "F":
+                rows = front
+            else:
+                rows = back
+        if not len(rows) == 1:
+            raise ValueError("More than one row left in the list " + str(rows))
+        row = rows[0]
+        columns = COLLUMS.copy()
+        for col in seat[7:]:
+            lower, upper = split_middle(columns)
+            if col == "L":
+                columns = lower
+            else:
+                columns = upper
+        if not len(columns) == 1:
+            raise ValueError("More than one cols left in the list " + str(columns))
+        col = columns[0]
+        seat = int(row) * 8 + int(col)
+        if seat > highest_seat:
+            highest_seat = seat
+        seats.append(seat)
+    return seats, highest_seat
 
 
 def part_2(data):
-    pass
+    seats = sorted(part_1(data)[0])
+    start = seats[0]
+    for seat in seats[1:]:
+        if seat - start != 1:
+            print(start, seat)
+            break
+        start = seat
+    return start + 1
 
 
 if __name__ == '__main__':
-    source = get_data("day-X.txt")
-    print(part_1(source))
-    print(part_2(source))
+    source = get_data("day-5.txt")
+    print("Part 1:", part_1(source)[1])  # 955
+    print("part 2:", part_2(source))  #
