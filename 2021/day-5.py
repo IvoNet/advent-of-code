0ccbeacb4febd970f6993a843f0c7676efb830cc
Ivo Woltring <ivo@ivonet.nl>
2021-12-05 08:07:23 +0100

    2021 day 5 part_1 finished

--- 2021/day-5.py
+++ 2021/day-5.py
@@ -7,14 +7,9 @@ __license__ = "Apache 2.0"
 __doc__ = """
 """
 
-import sys
 import unittest
 
 from ivonet import read_data
-
-
-def get_line(startx, starty, endx, endy):
-    coords = []
 
 
 def grid_adder(grid, x, y):
@@ -24,7 +19,7 @@ def grid_adder(grid, x, y):
         grid[(x, y)] = 1
 
 
-def parse(data):
+def part_1(data):
     rows = data.split("\n")
     grid = {}
     for row in rows:
@@ -32,32 +27,56 @@ def parse(data):
         start_x, start_y = map(int, start.split(","))
         end_x, end_y = map(int, end.split(","))
         if not (start_x == end_x or start_y == end_y):
-            print("diagonal:", start_x, start_y, end_x, end_y)
+            # print("diagonal:", start_x, start_y, end_x, end_y)
             continue
+        step = 1
         if start_x == end_x:
-            for y in range(start_y, end_y):
+            if start_y > end_y:
+                step = -1
+            for y in range(start_y, end_y, step):
                 grid_adder(grid, start_x, y)
-        else:  # start_y == end_y
-            for x in range(start_x, end_x):
+        else:  # start_y == end_y:
+            if start_x > end_x:
+                step = -1
+            for x in range(start_x, end_x, step):
                 grid_adder(grid, x, start_y)
         grid_adder(grid, end_x, end_y)
-        print(start_x, start_y, end_x, end_y, grid)
+        # print(start_x, start_y, end_x, end_y, grid)
     count = 0
     for value in grid.values():
         if value >= 2:
             count += 1
-    print(grid.values())
-    print(count)
-    sys.exit(1)
     return count
 
 
-def part_1(data):
-    return parse(data)
-
-
 def part_2(data):
-    pass
+    rows = data.split("\n")
+    grid = {}
+    for row in rows:
+        start, end = row.split(" -> ")
+        start_x, start_y = map(int, start.split(","))
+        end_x, end_y = map(int, end.split(","))
+        step = 1
+        if start_x == end_x:
+            if start_y > end_y:
+                step = -1
+            for y in range(start_y, end_y, step):
+                grid_adder(grid, start_x, y)
+            grid_adder(grid, end_x, end_y)
+        elif start_y == end_y:
+            if start_x > end_x:
+                step = -1
+            for x in range(start_x, end_x, step):
+                grid_adder(grid, x, start_y)
+            grid_adder(grid, end_x, end_y)
+        else:  # diagonale
+            pass
+        # print(start_x, start_y, end_x, end_y, grid)
+    count = 0
+    for value in grid.values():
+        if value >= 2:
+            count += 1
+    return count
 
 
 class UnitTests(unittest.TestCase):
@@ -74,11 +93,11 @@ class UnitTests(unittest.TestCase):
 3,4 -> 1,4
 0,0 -> 8,8
 5,5 -> 8,2"""
-        self.assertEqual(part_1(source), 5)
-        self.assertEqual(part_2(source), None)
+        self.assertEqual(5, part_1(source))
+        self.assertEqual(12, part_2(source))
 
     def test_part_1(self):
-        self.assertEqual(part_1(self.source), 0)
+        self.assertEqual(5306, part_1(self.source))
 
     def test_part_2(self):
         self.assertEqual(part_2(self.source), 0)
