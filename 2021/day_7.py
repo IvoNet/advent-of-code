133b0d9f93fbc02ff74eb94a9271acc51fd5f081
Ivo Woltring <ivo@ivonet.nl>
2021-12-07 06:27:34 +0100

    2021 day 7 start

---
+++ 2021/day_7.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+#  -*- coding: utf-8 -*-
+__author__ = "Ivo Woltring"
+__revised__ = "$revised: 01/12/2021 10:39$"
+__copyright__ = "Copyright (c) 2021 Ivo Woltring"
+__license__ = "Apache 2.0"
+__doc__ = """
+
+"""
+
+import unittest
+
+from ivonet import get_data
+
+
+def part_1(data):
+    pass
+
+
+def part_2(data):
+    pass
+
+
+class UnitTests(unittest.TestCase):
+    source = get_data("day_7.txt")
+    test_source = """""".split("\n")
+
+    def test_example_data_part_1(self):
+        self.assertEqual(None, part_1(self.test_source))
+
+    def test_example_data_part_2(self):
+        self.assertEqual(None, part_2(self.test_source))
+
+    def test_part_1(self):
+        self.assertEqual(None, part_1(self.source))
+
+    def test_part_2(self):
+        self.assertEqual(None, part_2(self.source))
+
+
+if __name__ == '__main__':
+    unittest.main()
