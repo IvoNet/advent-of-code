792e06962d91d13456b18dcef027f32cbbdb01ac
Ivo Woltring <ivo@ivonet.nl>
2021-12-10 20:50:43 +0100

    2021 day 11 prepare

--- 2021/day_11.py
+++ 2021/day_11.py
@@ -9,11 +9,12 @@ __doc__ = """
 """
 
 import sys
-import unittest
 
 from ivonet.files import read_rows
 
 sys.dont_write_bytecode = True
+
+import unittest
 
 
 def part_1(source):
@@ -25,17 +26,17 @@ def part_2(source):
 
 
 class UnitTests(unittest.TestCase):
-    source = read_rows("day_X.txt")
+    source = read_rows("day_11.txt")
     test_source = read_rows("""""")
 
     def test_example_data_part_1(self):
         self.assertEqual(None, part_1(self.test_source))
 
-    def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
-
     def test_part_1(self):
         self.assertEqual(None, part_1(self.source))
+
+    def test_example_data_part_2(self):
+        self.assertEqual(None, part_2(self.test_source))
 
     def test_part_2(self):
         self.assertEqual(None, part_2(self.source))
