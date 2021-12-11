dc7b9bffb3da80f8a2987283ee6a361da6fcc066
Ivo Woltring <ivo@ivonet.nl>
2021-12-11 06:30:22 +0100

    2021 day 11 wip

--- 2021/day_11.py
+++ 2021/day_11.py
@@ -9,28 +9,61 @@ __doc__ = """
 """
 
 import sys
+from pprint import pprint
+from queue import Queue
 
-from ivonet.files import read_rows
+from ivonet.files import read_int_matrix
 
 sys.dont_write_bytecode = True
 
 import unittest
 
 
+def step_matrix(source):
+    matrix = source.copy()
+    height = len(matrix)
+    width = len(matrix[0])
+    flash_q = Queue()
+    for h in range(height):
+        for w in range(width):
+            matrix[h][w] = matrix[h][w] + 1
+            if matrix[h][w] > 9:
+                flash_q.put((h, w))
+    return matrix, flash_q
+
+
 def part_1(source):
-    pass
+    flashes = 0
+    matrix = source.copy()
+    height = len(matrix)
+    width = len(matrix[0])
+    for step in range(100):
+        matrix, flash_q = step_matrix(matrix)
+        pprint(matrix)
+        while not flash_q.empty():
+            h, w = flash_q.get()
+            nb =
 
 
-def part_2(source):
+def part_2(matrix):
     pass
 
 
 class UnitTests(unittest.TestCase):
-    source = read_rows("day_11.txt")
-    test_source = read_rows("""""")
+    source = read_int_matrix("day_11.txt")
+    test_source = read_int_matrix("""5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526""")
 
     def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+        self.assertEqual(1656, part_1(self.test_source))
 
     def test_part_1(self):
         self.assertEqual(None, part_1(self.source))
