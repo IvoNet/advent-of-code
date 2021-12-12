86579274c9c3fe725c83b8aaa4c4b6985aec50dc
Ivo Woltring <ivo@ivonet.nl>
2021-12-12 08:04:03 +0100

    2021 day 12 part 1

--- 2021/day_12.py
+++ 2021/day_12.py
@@ -10,14 +10,140 @@ __doc__ = """
 
 import sys
 import unittest
+from collections import defaultdict
 
 from ivonet.files import read_rows
 
 sys.dont_write_bytecode = True
 
 
+class Graph:
+    def __init__(self, vertices):
+        # No. of vertices
+        self.V = vertices
+
+        # default dictionary to store graph
+        self.graph = defaultdict(list)
+
+    # function to add an edge to graph
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    def printAllPathsUtil(self, u, d, visited, path):
+        """A recursive function to print all paths from 'u' to 'd'.
+         visited[] keeps track of vertices in current path.
+         path[] stores actual vertices and path_index is current
+         index in path[]"""
+
+        # Mark the current node as visited and store in path
+        visited[u] = True
+        path.append(u)
+
+        # If current vertex is same as destination, then print
+        # current path[]
+        if u == d:
+            print
+            path
+        else:
+            # If current vertex is not destination
+            # Recur for all the vertices adjacent to this vertex
+            for i in self.graph[u]:
+                if visited[i] == False:
+                    self.printAllPathsUtil(i, d, visited, path)
+
+        # Remove current vertex from path[] and mark it as unvisited
+        path.pop()
+        visited[u] = False
+
+    # Prints all paths from 's' to 'd'
+    def printAllPaths(self, s, d):
+
+        # Mark all the vertices as not visited
+        visited = [False] * (self.V)
+
+        # Create an array to store paths
+        path = []
+
+        # Call the recursive helper function to print all paths
+        self.printAllPathsUtil(s, d, visited, path)
+
+
+class Node:
+
+    def __init__(self, name: str) -> None:
+        self.small = True
+        if name != name.lower():
+            self.small = False
+        self.name = name
+        self.visited = 0
+        self.visitable = 1
+
+    def __str__(self) -> str:
+        return repr(self.name)
+
+    def __repr__(self) -> str:
+        return repr(self.name)
+
+    def __hash__(self) -> int:
+        return hash(repr(self))
+
+    def __eq__(self, o: object) -> bool:
+        return repr(self) == repr(o)
+
+
+class Route:
+
+    def __init__(self) -> None:
+        self.nodes = defaultdict(list)
+        self.small = []
+
+    def add(self, s: str):
+        a, b = s.split("-")
+        aa = Node(a)
+        bb = Node(b)
+        self.nodes[aa].append(bb)
+        self.nodes[bb].append(aa)
+        if aa.small:
+            self.small.append(aa)
+        if bb.small:
+            self.small.append(bb)
+
+    def find_all_paths(self, start=Node("start"), end=Node("end"), visited=[], path=[]):
+        path = path + [start]
+        if start.small:
+            visited = visited + [start]
+        if start == end:
+            return [path]
+        if start not in self.nodes:
+            return []
+        paths = []
+        for node in self.nodes[start]:
+            if node not in visited:
+                newpaths = self.find_all_paths(node, end, visited, path)
+                for newpath in newpaths:
+                    paths.append(newpath)
+        return paths
+
+    def find_all_paths_with_one_small_twice(self, start=Node("start"), end=Node("end"), visited=[], path=[]):
+        paths = []
+        for node in self.small:
+            node.visitable = 2
+            new_paths = self.find_all_paths(node, end)
+
+    def __str__(self) -> str:
+        return repr(self.nodes)
+
+    def __repr__(self) -> str:
+        return repr(self.nodes)
+
+
 def part_1(source):
-    pass
+    routes = Route()
+    for x in source:
+        routes.add(x)
+    a = routes.find_all_paths()
+    print(a)
+    return len(a)
 
 
 def part_2(source):
@@ -35,13 +161,7 @@ A-b
 b-d
 A-end
 b-end""")
-        self.test_source_bigger = read_rows("""start-A
-start-b
-A-c
-A-b
-b-d
-A-end
-b-enddc-end
+        self.test_source_bigger = read_rows("""dc-end
 HN-start
 start-kj
 dc-start
@@ -70,17 +190,20 @@ zg-he
 pj-fs
 start-RW""")
 
-    def test_example_data_part_2_smallest(self):
-        self.assertEqual(16, part_2(self.test_source_smallest))
+    def test_example_data_part_1_smallest(self):
+        self.assertEqual(10, part_1(self.test_source_smallest))
 
-    def test_example_data_part_1_small(self):
+    def test_example_data_part_1_biggerl(self):
         self.assertEqual(19, part_1(self.test_source_bigger))
 
     def test_example_data_part_1(self):
         self.assertEqual(226, part_1(self.test_source))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
+        self.assertEqual(3410, part_1(self.source))
+
+    def test_example_data_part_2(self):
+        self.assertEqual(36, part_2(self.test_source_smallest))
 
     def test_part_2(self):
         self.assertEqual(None, part_2(self.source))
