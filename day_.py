1454493d680a824a1d03a17176d5aed14d2b26a5
Ivo Woltring <ivo@ivonet.nl>
2021-12-13 23:12:20 +0100

    2021 day 14 preparations

--- day_.py
+++ day_.py
@@ -19,11 +19,11 @@ sys.dont_write_bytecode = True
 
 
 def part_1(source):
-    pass
+    return 0
 
 
 def part_2(source):
-    pass
+    return 0
 
 
 class UnitTests(unittest.TestCase):
@@ -36,11 +36,11 @@ class UnitTests(unittest.TestCase):
     def test_example_data_part_1(self):
         self.assertEqual(None, part_1(self.test_source))
 
-    def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
-
     def test_part_1(self):
         self.assertEqual(None, part_1(self.source))
+
+    def test_example_data_part_2(self):
+        self.assertEqual(None, part_2(self.test_source))
 
     def test_part_2(self):
         self.assertEqual(None, part_2(self.source))
