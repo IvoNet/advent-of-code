a68400b2c84d284dcb8046d63ce633498cb88106
Ivo Woltring <ivo@ivonet.nl>
2021-12-17 10:15:14 +0100

    2021 day 17 part 2 wip

--- 2021/day_17.py
+++ 2021/day_17.py
@@ -158,10 +158,10 @@ sys.dont_write_bytecode = True
 def step(position: tuple[int, int], trajectory: tuple[int, int]) -> tuple[tuple[int, int], tuple[int, int]]:
     px, py = position
     tx, ty = trajectory
-    tx += -1 if tx > 0 else 1
-    ty -= 1
     px += tx
     py += ty
+    tx += -1 if tx > 0 else 1
+    ty -= 1
     return (px, py), (tx, ty)
 
 
@@ -186,14 +186,27 @@ def beyond_area(area: tuple[tuple[int, int], tuple[int, int]], point: tuple[int,
     return px > bx or py < by
 
 
+def trace(area, position, trajectory):
+    print(f"start[{position}], trajectory[{trajectory}]")
+    if area_contains(area, position):
+        print("Hit!")
+        return
+    if beyond_area(area, position):
+        print("Fail!")
+        return
+    position, trajectory = step(position, trajectory)
+    return trace(area, position, trajectory)
+
+
 def part_1(source):  # 6,9
     target = target_area(source)
+    top_left, bottom_right = target
     current = (0, 0)
     best = (0, 0)
     highest = (0, 0)
     total_steps = 0
-    for x in range(0, 1000):
-        for y in range(0, 1000):
+    for x in range(0, bottom_right[0] + 1):
+        for y in range(1000, -1000, -1):
             current = (x, y)
             steps = 0
             hpoint = (0, 0)
@@ -206,7 +219,7 @@ def part_1(source):  # 6,9
                 steps += 1
                 # print(steps, position)
                 if area_contains(target, position):
-                    # print(f"start[{current}], position[{position}], steps[{steps}], highest[{hpoint}]")
+                    print(f"start[{current}], position[{position}], steps[{steps}], highest[{hpoint}]")
                     if hpoint[1] > highest[1]:
                         highest = hpoint
                         best = (x, y)
@@ -221,23 +234,24 @@ def part_1(source):  # 6,9
 
 def part_2(source):
     target = target_area(source)
-    current = (0, 0)
+    top_left, bottom_right = target
+    # trace(target, (0, 0), (7, 2))
+    good_list = []
     good_start_velocities = set()
-    for x in range(0, 1000):
-        for y in range(-1000, 1001):
+    for x in range(0, bottom_right[0] + 1):
+        for y in range(bottom_right[1] - 1, 1000):
             current = (x, y)
-            steps = 0
             position = (0, 0)
             trajectory = (x, y)
             while not beyond_area(target, position):
                 position, trajectory = step(position, trajectory)
-                steps += 1
                 if area_contains(target, position):
                     good_start_velocities.add(current)
-                    break
+                    good_list.append(current)
 
-    print(good_start_velocities)
-    return len(good_start_velocities) + 1
+    print(good_start_velocities, good_list)
+    print(len(good_start_velocities), len(good_list))
+    return good_start_velocities
 
 
 class UnitTests(unittest.TestCase):
@@ -246,6 +260,21 @@ class UnitTests(unittest.TestCase):
         day = ints(Path(__file__).name)[0]
         self.source = read_data(f"day_{day}.txt")
         self.test_source = read_data("""target area: x=20..30, y=-10..-5""")
+        self.test_coordinates = (
+            (23, -10), (25, -9), (27, -5), (29, -6), (22, -6), (21, -7), (9, 0), (27, -7), (24, -5),
+            (25, -7), (26, -6), (25, -5), (6, 8), (11, -2), (20, -5), (29, -10), (6, 3), (28, -7),
+            (8, 0), (30, -6), (29, -8), (20, -10), (6, 7), (6, 4), (6, 1), (14, -4), (21, -6),
+            (26, -10), (7, -1), (7, 7), (8, -1), (21, -9), (6, 2), (20, -7), (30, -10), (14, -3),
+            (20, -8), (13, -2), (7, 3), (28, -8), (29, -9), (15, -3), (22, -5), (26, -8), (25, -8),
+            (25, -6), (15, -4), (9, -2), (15, -2), (12, -2), (28, -9), (12, -3), (24, -6), (23, -7),
+            (25, -10), (7, 8), (11, -3), (26, -7), (7, 1), (23, -9), (6, 0), (22, -10), (27, -6),
+            (8, 1), (22, -8), (13, -4), (7, 6), (28, -6), (11, -4), (12, -4), (26, -9), (7, 4),
+            (24, -10), (23, -8), (30, -8), (7, 0), (9, -1), (10, -1), (26, -5), (22, -9), (6, 5),
+            (7, 5), (23, -6), (28, -10), (10, -2), (11, -1), (20, -9), (14, -2), (29, -7), (13, -3),
+            (23, -5), (24, -8), (27, -9), (30, -7), (28, -5), (21, -10), (7, 9), (6, 6), (21, -5),
+            (27, -10), (7, 2), (30, -9), (21, -8), (22, -7), (24, -9), (20, -6), (6, 9), (29, -5),
+            (8, -2), (27, -8), (30, -5), (24, -7),
+        )
 
     def test_example_data_part_1(self):
         self.assertEqual(45, part_1(self.test_source))
@@ -254,7 +283,7 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(4186, part_1(self.source))
 
     def test_example_data_part_2(self):
-        self.assertEqual(112, part_2(self.test_source))
+        self.assertEqual(sorted(self.test_coordinates), sorted(part_2(self.test_source)))
 
     def test_part_2(self):
         self.assertEqual(None, part_2(self.source))
