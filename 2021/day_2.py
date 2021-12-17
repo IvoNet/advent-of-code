616fb73f24fdc112ac413c21dd39fd3f470ffb77
Ivo Woltring <ivo@ivonet.nl>
2021-12-17 18:04:46 +0100

    refactoring day 2

--- 2021/day_2.py
+++ 2021/day_2.py
@@ -83,11 +83,14 @@ and depth you would have after following the planned course. What do you get if
 you multiply your final horizontal position by your final depth?
 """
 
+import unittest
+from pathlib import Path
+
 from ivonet.files import read_rows
+from ivonet.iter import ints
 
 
-def part_1():
-    rows = read_rows("day_2.txt")
+def part_1(rows):
     x = 0
     y = 0
     for course in rows:
@@ -102,8 +105,7 @@ def part_1():
     return x * y
 
 
-def part_2():
-    rows = read_rows("day_2.txt")
+def part_2(rows):
     x = 0
     y = 0
     aim = 0
@@ -120,6 +122,30 @@ def part_2():
     return x * y
 
 
+class UnitTests(unittest.TestCase):
+
+    def setUp(self) -> None:
+        day = ints(Path(__file__).name)[0]
+        self.source = read_rows(f"day_{day}.txt")
+        self.test_source = read_rows("""forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2""")
+
+    def test_example_data_part_1(self):
+        self.assertEqual(150, part_1(self.test_source))
+
+    def test_part_1(self):
+        self.assertEqual(2019945, part_1(self.source))
+
+    def test_example_data_part_2(self):
+        self.assertEqual(900, part_2(self.test_source))
+
+    def test_part_2(self):
+        self.assertEqual(1599311480, part_2(self.source))
+
+
 if __name__ == '__main__':
-    print(part_1())  # 2019945
-    print(part_2())  # 1599311480
+    unittest.main()
