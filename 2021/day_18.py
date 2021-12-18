71b493eb0671719912ce0f8f1f9360a410fbda5f
Ivo Woltring <ivo@ivonet.nl>
2021-12-18 20:01:45 +0100

    2021 day 18 - refactoring

--- 2021/day_18.py
+++ 2021/day_18.py
@@ -176,12 +176,26 @@ def _(*args, end="\n"):
 
 
 class Pair:
-    left: int | None = None
-    right: int | None = None
-    left_pair: Pair | None = None
-    right_pair: Pair | None = None
-    depth: int = 0
-    parent: Pair | None = None
+    def __init__(self, left: int | None = None,
+                 right: int | None = None,
+                 left_pair: Pair | None = None,
+                 right_pair: Pair | None = None,
+                 parent: Pair | None = None,
+                 depth: int = 0) -> None:
+        self.left: int | None = left
+        self.right: int | None = right
+        self.left_pair: Pair | None = left_pair
+        self.right_pair: Pair | None = right_pair
+        self.parent: Pair | None = parent
+        self.depth: int = depth
+
+    def __str__(self) -> str:
+        return visualize(self)
+
+    def __repr__(self) -> str:
+        return f"Pair(left={self.left}, right={self.right}, " \
+               f"left_pair={self.left_pair}, right_pair={self.right_pair}, " \
+               f"depth={self.depth}, parent={'...' if self.parent else None})"
 
 
 def visualize(sfn: Pair):
@@ -210,35 +224,45 @@ def string_to_snailfish_number(value) -> list:
 def parse(sfn: list, depth: int = 0, parent: [Pair | None] = None) -> [Pair | None]:
     """Parse the lists into a pair tree
     """
-    ret = Pair()
-    ret.depth = depth
-    ret.parent = parent
+    _("parse:", sfn)
+    ret = Pair(parent=parent, depth=depth)
     # left (zero index)
     if type(sfn[0]) == list:
+        _("parse:", "left_pair")
         ret.left_pair = parse(sfn[0], depth + 1, ret)
     else:  # plain number
+        _("parse:", "left value", sfn[0])
         ret.left = sfn[0]
     # right (index 1)
     if type(sfn[1]) == list:
+        _("parse:", "right_pair")
         ret.right_pair = parse(sfn[1], depth + 1, ret)
     else:
+        _("parse:", "right value", sfn[1])
         ret.right = sfn[1]
 
     return ret
 
 
 def update_depths(sfn: Pair) -> Pair:
+    """Updates the depths of all the pairs when moved down a level (after addition)
+    """
     sfn.depth += 1
+    _("update_depths:", sfn)
     if sfn.left_pair is not None:
+        _("update_depths:", "left_pair")
         sfn.left_pair = update_depths(sfn.left_pair)
     if sfn.right_pair is not None:
+        _("update_depths:", "right_pair")
         sfn.right_pair = update_depths(sfn.right_pair)
     return sfn
 
 
 def update_down_right(sfn: Pair, value: int):
-    """Updating down right"""
-    # _("Updating down right")
+    """Updating down right
+    - add the value if it its left value already has meaning
+    - otherwise walk down the left side until you can do this
+    """
     if sfn.left is not None:
         sfn.left += value
     else:
@@ -247,21 +271,24 @@ def update_down_right(sfn: Pair, value: int):
 
 def update_up_right(sfn: Pair, value: int):
     """Update up right
+    - now you have to look down as we need to
     """
-    # _("Update up right")
+    _("update_up_right:", visualize(sfn), value)
     parent: Pair = sfn.parent
     if parent is None:
-        _("Update up right: No parent found")
+        _("update_up_right:", "No parent")
         return
     if parent.left_pair == sfn:
+        _("update_up_right:", "left_pair == sfn")
         if parent.right is not None:
+            _("update_up_right:", "setting parent.right =", value)
             parent.right += value
         else:
             update_down_right(parent.right_pair, value)
     elif parent.right_pair == sfn:
         update_up_right(parent, value)
     else:
-        _("Update up right: No parent to update?!")
+        _("update_up_right:", "No parent to update")
 
 
 def update_down_left(sfn: Pair, value: int):
@@ -282,17 +309,19 @@ def update_up_left(sfn: Pair, value: int):
     """
     parent: Pair = sfn.parent
     if parent is None:
-        _("Update up left: No parent found")
+        _("update_up_left:", "No parent")
         return
     if parent.right_pair == sfn:
+        _("update_up_left:", "right_pair == sfn")
         if parent.left is not None:
+            _("update_up_left:", "setting parent.left =", value)
             parent.left += value
         else:
             update_down_left(parent.left_pair, value)
     elif parent.left_pair == sfn:
         update_up_left(parent, value)
     else:
-        _("Update up left: No parent to update?!")
+        _("update_up_left:", "No parent to update")
 
 
 def explode(sfn: Pair) -> bool:
@@ -301,7 +330,9 @@ def explode(sfn: Pair) -> bool:
     - a depth >= 4
     - our left and right values not empty
     """
+    _("explode:", str(sfn))
     if sfn.depth >= 4 and sfn.left is not None and sfn.right is not None:
+        _("explode:", "Depth found:", repr(sfn))
         # should there always be a parent? yes as we have a depth of more than 4
         update_up_left(sfn, sfn.left)
         update_up_right(sfn, sfn.right)
@@ -313,7 +344,7 @@ def explode(sfn: Pair) -> bool:
             sfn.parent.right_pair = None
             sfn.parent.right = 0
         else:
-            _("Explode: no parent to delete?!", sfn)
+            _("Explode: no parent to clean", sfn)
             return False
         return True
     if sfn.left_pair is not None and explode(sfn.left_pair):
@@ -325,51 +356,54 @@ def explode(sfn: Pair) -> bool:
 
 def split_it(sfn: Pair) -> bool:
     """Splitting it
-    - from left to right
-    - act on ourselves
-    - redepth?!
+    - from left to right the first one bigger than 9
+        - left div 2 (rounded down)
+        - right div 2 (rounded up) -> effectively sfn.right - left
+    - act on ourselves first
+    the returning bool tells if a split has taken place or not
     """
+    _("split:", sfn)
     if sfn.left is not None and sfn.left > 9:
+        _("split:", "left split:", sfn)
         left = sfn.left // 2
         right = sfn.left - left
-        sfn.left_pair = Pair()
-        sfn.left_pair.left = left
-        sfn.left_pair.right = right
-        sfn.left_pair.depth = sfn.depth + 1
-        sfn.left_pair.parent = sfn
+        sfn.left_pair = Pair(left=left, right=right, parent=sfn, depth=sfn.depth + 1)
         sfn.left = None
+        _("split:", "becomes   :", sfn)
         return True
-    if sfn.left_pair is not None and split_it(sfn.left_pair):
+    if sfn.left_pair is not None and split_it(sfn.left_pair):  # Possible recursion here
         return True
     if sfn.right is not None and sfn.right > 9:
+        _("split:", "right split: ", sfn)
         left = sfn.right // 2
         right = sfn.right - left
-        sfn.right_pair = Pair()
-        sfn.right_pair.left = left
-        sfn.right_pair.right = right
-        sfn.right_pair.depth = sfn.depth + 1
-        sfn.right_pair.parent = sfn
+        sfn.right_pair = Pair(left=left, right=right, parent=sfn, depth=sfn.depth + 1)
         sfn.right = None
+        _("split:", "becomes    :", sfn)
         return True
-    if sfn.right_pair is not None and split_it(sfn.right_pair):
+    if sfn.right_pair is not None and split_it(sfn.right_pair):  # Possible recursion here
         return True
     return False
 
 
-def sfn_reduce(sfn: Pair) -> Pair:
+def sfn_reduce(sfn: Pair) -> None:
     """Starfish number reduce function
     - one action at the time
     - explode has precedence over split
     - repeat until no reduce actions left
       this happens when split returns false
     """
+    _("reduce:", sfn)
     while True:
         action_performed = explode(sfn)
         if action_performed:
+            _("reduce:", "exploded:", sfn)
             continue
         action_performed = split_it(sfn)
         if not action_performed:
             break
+        else:
+            _("reduce:", "split:", sfn)
 
 
 def addition(left: Pair, right: Pair):
@@ -410,13 +444,8 @@ def addition(left: Pair, right: Pair):
         - top.left_pair.parent := top
         - top.right_pair.parent := top
     """
-    top = Pair()
-    top.left = None
-    top.right = None
-    top.parent = None
-    top.depth = 0
-    top.left_pair = update_depths(left)
-    top.right_pair = update_depths(right)
+    _("addition:", left, "+", right)
+    top = Pair(left_pair=update_depths(left), right_pair=update_depths(right))
 
     top.left_pair.parent = top
     top.right_pair.parent = top
@@ -439,18 +468,27 @@ def magnitude(sfn: Pair) -> int:
     return total
 
 
-def part_1(source):
-    snailfish_numbers: list[Pair] = []
-    for s in source:
-        snailfish_numbers.append(parse(string_to_snailfish_number(s), depth=0, parent=None))
-    _("Numbers:", snailfish_numbers)
-
+def sum_all(snailfish_numbers):
     sf_sum = snailfish_numbers[0]
     for i, sfn in enumerate(snailfish_numbers):
         if i == 0:  # skip as we already assigned it to have a starting point
             continue
-        _("Addition of", sf_sum, "and", sfn)
         sf_sum = addition(sf_sum, sfn)
+    _("sum_all:", "result", sf_sum)
+    return sf_sum
+
+
+def parse_all(source):
+    snailfish_numbers: list[Pair] = []
+    for s in source:
+        snailfish_numbers.append(parse(string_to_snailfish_number(s), depth=0, parent=None))
+    return snailfish_numbers
+
+
+def part_1(source):
+    snailfish_numbers = parse_all(source)
+
+    sf_sum = sum_all(snailfish_numbers)
 
     return magnitude(sf_sum)
 
@@ -488,18 +526,99 @@ class UnitTests(unittest.TestCase):
 [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
 [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]""")
 
+    def test_explode_1(self):
+        pair = parse([[[[[9, 8], 1], 2], 3], 4])
+        self.assertTrue(explode(pair))
+        self.assertEqual("[[[[0,9],2],3],4]", visualize(pair))
+
+    def test_explode_2(self):
+        pair = parse([7, [6, [5, [4, [3, 2]]]]])
+        self.assertTrue(explode(pair))
+        self.assertEqual("[7,[6,[5,[7,0]]]]", visualize(pair))
+
+    def test_explode_3(self):
+        pair = parse([[6, [5, [4, [3, 2]]]], 1])
+        self.assertTrue(explode(pair))
+        self.assertEqual("[[6,[5,[7,0]]],3]", visualize(pair))
+
+    def test_explode_4(self):
+        pair = parse([[3, [2, [1, [7, 3]]]], [6, [5, [4, [3, 2]]]]])
+        self.assertTrue(explode(pair))
+        self.assertEqual("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", visualize(pair))
+
+    def test_explode_5(self):
+        pair = parse([[3, [2, [8, 0]]], [9, [5, [4, [3, 2]]]]])
+        self.assertTrue(explode(pair))
+        self.assertEqual("[[3,[2,[8,0]]],[9,[5,[7,0]]]]", visualize(pair))
+
+    def test_split_1(self):
+        pair = parse([[[[0, 7], 4], [15, [0, 13]]], [1, 1]])
+        self.assertTrue(True, split_it(pair))
+        self.assertEqual("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]", visualize(pair))
+        self.assertTrue(True, split_it(pair))
+        self.assertEqual("[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]", visualize(pair))
+
     def test_split_(self):
         pair = parse([[[[0, 7], 4], [15, [0, 13]]], [1, 1]])
         self.assertTrue(True, split_it(pair))
+        self.assertEqual("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]", visualize(pair))
 
-    def test_magnitude(self):
+    def test_magnitude_1(self):
         sfn = parse([[[[6, 6], [7, 6]], [[7, 7], [7, 0]]], [[[7, 7], [7, 7]], [[7, 8], [9, 9]]]])
         self.assertEqual(4140, magnitude(sfn))
+
+    def test_magnitude_2(self):
+        sfn = parse([[1, 2], [[3, 4], 5]])
+        self.assertEqual(143, magnitude(sfn))
+
+    def test_magnitude_3(self):
+        sfn = parse([[[[1, 1], [2, 2]], [3, 3]], [4, 4]])
+        self.assertEqual(445, magnitude(sfn))
+
+    def test_magnitude_4(self):
+        sfn = parse([[[[0, 7], 4], [[7, 8], [6, 0]]], [8, 1]])
+        self.assertEqual(1384, magnitude(sfn))
+
+    def test_magnitude_5(self):
+        sfn = parse([[[[3, 0], [5, 3]], [4, 4]], [5, 5]])
+        self.assertEqual(791, magnitude(sfn))
+
+    def test_magnitude_6(self):
+        sfn = parse([[[[5, 0], [7, 4]], [5, 5]], [6, 6]])
+        self.assertEqual(1137, magnitude(sfn))
+
+    def test_magnitude_(self):
+        sfn = parse([[[[8, 7], [7, 7]], [[8, 6], [7, 7]]], [[[0, 7], [6, 6]], [8, 7]]])
+        self.assertEqual(3488, magnitude(sfn))
 
     def test_visualize(self):
         s = "[[[[7,8],[6,6]],[[6,0],[7,7]]],[[[7,8],[8,8]],[[7,9],[0,6]]]]"
         pair = parse(string_to_snailfish_number(s))
         self.assertEqual(s, visualize(pair))
+
+    def test_small_data_set(self):
+        s = parse_all(read_rows("""[1,1]
+[2,2]
+[3,3]
+[4,4]"""))
+        self.assertEqual("[[[[1,1],[2,2]],[3,3]],[4,4]]", visualize(sum_all(s)))
+
+    def test_small_data_set_1(self):
+        s = parse_all(read_rows("""[1,1]
+[2,2]
+[3,3]
+[4,4]
+[5,5]"""))
+        self.assertEqual("[[[[3,0],[5,3]],[4,4]],[5,5]]", visualize(sum_all(s)))
+
+    def test_small_data_set_2(self):
+        s = parse_all(read_rows("""[1,1]
+[2,2]
+[3,3]
+[4,4]
+[5,5]
+[6,6]"""))
+        self.assertEqual("[[[[5,0],[7,4]],[5,5]],[6,6]]", visualize(sum_all(s)))
 
     def test_example_data_part_1(self):
         self.assertEqual(4140, part_1(self.test_source))
