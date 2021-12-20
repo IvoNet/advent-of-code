9adc3da9d66dbded76fb97b4f7391fce551be406
Ivo Woltring <ivo@ivonet.nl>
2021-12-20 11:52:55 +0100

    2021 day 20 done - reverted back to my binary notation - cleanup of doc

--- 2021/day_20.py
+++ 2021/day_20.py
@@ -8,9 +8,13 @@ __copyright__ = "Copyright (c) 2021 Ivo Woltring"
 __license__ = "Apache 2.0"
 __doc__ = """
 --- Day 20: Trench Map ---
-With the scanners fully deployed, you turn their attention to mapping the floor of the ocean trench.
 
-When you get back the image from the scanners, it seems to just be random noise. Perhaps you can combine an image enhancement algorithm and the input image (your puzzle input) to clean it up a little.
+With the scanners fully deployed, you turn their attention to mapping the 
+floor of the ocean trench.
+
+When you get back the image from the scanners, it seems to just be random noise. 
+Perhaps you can combine an image enhancement algorithm and the input image 
+(your puzzle input) to clean it up a little.
 
 For example:
 
@@ -27,31 +31,59 @@ For example:
 ##..#
 ..#..
 ..###
-The first section is the image enhancement algorithm. It is normally given on a single line, but it has been wrapped to multiple lines in this example for legibility. The second section is the input image, a two-dimensional grid of light pixels (#) and dark pixels (.).
+The first section is the image enhancement algorithm. It is normally given on a single 
+line, but it has been wrapped to multiple lines in this example for legibility. 
+The second section is the input image, a two-dimensional grid of light pixels 
+(#) and dark pixels (.).
 
-The image enhancement algorithm describes how to enhance an image by simultaneously converting all pixels in the input image into an output image. Each pixel of the output image is determined by looking at a 3x3 square of pixels centered on the corresponding input image pixel. So, to determine the value of the pixel at (5,10) in the output image, nine pixels from the input image need to be considered: (4,9), (4,10), (4,11), (5,9), (5,10), (5,11), (6,9), (6,10), and (6,11). These nine input pixels are combined into a single binary number that is used as an index in the image enhancement algorithm string.
+The image enhancement algorithm describes how to enhance an image by simultaneously
+converting all pixels in the input image into an output image. Each pixel of the 
+output image is determined by looking at a 3x3 square of pixels centered on the 
+corresponding input image pixel. So, to determine the value of the pixel at (5,10) 
+in the output image, nine pixels from the input image need to be considered: 
+(4,9), (4,10), (4,11), (5,9), (5,10), (5,11), (6,9), (6,10), and (6,11). 
+These nine input pixels are combined into a single binary number that is used as 
+an index in the image enhancement algorithm string.
 
-For example, to determine the output pixel that corresponds to the very middle pixel of the input image, the nine pixels marked by [...] would need to be considered:
+For example, to determine the output pixel that corresponds to the very middle 
+pixel of the input image, the nine pixels marked by [...] would need to 
+be considered:
 
 # . . # .
 #[. . .].
 #[# . .]#
 .[. # .].
 . . # # #
-Starting from the top-left and reading across each row, these pixels are ..., then #.., then .#.; combining these forms ...#...#.. By turning dark pixels (.) into 0 and light pixels (#) into 1, the binary number 000100010 can be formed, which is 34 in decimal.
 
-The image enhancement algorithm string is exactly 512 characters long, enough to match every possible 9-bit binary number. The first few characters of the string (numbered starting from zero) are as follows:
+Starting from the top-left and reading across each row, these pixels are ..., 
+then #.., then .#.; combining these forms ...#...#.. By turning dark 
+pixels (.) into 0 and light pixels (#) into 1, the binary number 000100010 
+can be formed, which is 34 in decimal.
+
+The image enhancement algorithm string is exactly 512 characters long, enough 
+to match every possible 9-bit binary number. The first few characters of 
+the string (numbered starting from zero) are as follows:
 
 0         10        20        30  34    40        50        60        70
 |         |         |         |   |     |         |         |         |
 ..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..##
-In the middle of this first group of characters, the character at index 34 can be found: #. So, the output pixel in the center of the output image should be #, a light pixel.
+
+In the middle of this first group of characters, the character at index 34 
+can be found: #. So, the output pixel in the center of the output image should 
+be #, a light pixel.
 
 This process can then be repeated to calculate every pixel of the output image.
 
-Through advances in imaging technology, the images being operated on here are infinite in size. Every pixel of the infinite output image needs to be calculated exactly based on the relevant pixels of the input image. The small input image you have is only a small region of the actual infinite input image; the rest of the input image consists of dark pixels (.). For the purposes of the example, to save on space, only a portion of the infinite-sized input and output images will be shown.
+Through advances in imaging technology, the images being operated on here are 
+infinite in size. Every pixel of the infinite output image needs to be calculated 
+exactly based on the relevant pixels of the input image. The small input image 
+you have is only a small region of the actual infinite input image; the rest of 
+the input image consists of dark pixels (.). For the purposes of the example, 
+to save on space, only a portion of the infinite-sized input and output images 
+will be shown.
 
-The starting input image, therefore, looks something like this, with more dark pixels (.) extending forever in every direction not shown here:
+The starting input image, therefore, looks something like this, with more dark 
+pixels (.) extending forever in every direction not shown here:
 
 ...............
 ...............
@@ -68,7 +100,9 @@ The starting input image, therefore, looks something like this, with more dark p
 ...............
 ...............
 ...............
-By applying the image enhancement algorithm to every pixel simultaneously, the following output image can be obtained:
+
+By applying the image enhancement algorithm to every pixel simultaneously, the following 
+output image can be obtained:
 
 ...............
 ...............
@@ -85,7 +119,8 @@ By applying the image enhancement algorithm to every pixel simultaneously, the f
 ...............
 ...............
 ...............
-Through further advances in imaging technology, the above output image can also be used as an input image! This allows it to be enhanced a second time:
+Through further advances in imaging technology, the above output image can also be used 
+as an input image! This allows it to be enhanced a second time:
 
 ...............
 ...............
@@ -102,17 +137,25 @@ Through further advances in imaging technology, the above output image can also 
 ...............
 ...............
 ...............
-Truly incredible - now the small details are really starting to come through. After enhancing the original input image twice, 35 pixels are lit.
 
-Start with the original input image and apply the image enhancement algorithm twice, being careful to account for the infinite size of the images. How many pixels are lit in the resulting image?
+Truly incredible - now the small details are really starting to come through. After 
+enhancing the original input image twice, 35 pixels are lit.
+
+Start with the original input image and apply the image enhancement algorithm twice, 
+being careful to account for the infinite size of the images. How many pixels are 
+lit in the resulting image?
 
 
 --- Part Two ---
-You still can't quite make out the details in the image. Maybe you just didn't enhance it enough.
 
-If you enhance the starting input image in the above example a total of 50 times, 3351 pixels are lit in the final output image.
+You still can't quite make out the details in the image. Maybe you just didn't 
+enhance it enough.
 
-Start again with the original input image and apply the image enhancement algorithm 50 times. How many pixels are lit in the resulting image?
+If you enhance the starting input image in the above example a total of 50 
+times, 3351 pixels are lit in the final output image.
+
+Start again with the original input image and apply the image enhancement 
+algorithm 50 times. How many pixels are lit in the resulting image?
 """
 
 import sys
@@ -121,12 +164,13 @@ from collections import defaultdict, OrderedDict
 from pathlib import Path
 from typing import NamedTuple
 
+from ivonet.calc import base_x_to_10
 from ivonet.files import read_rows
 from ivonet.iter import ints
 
 sys.dont_write_bytecode = True
 
-DEBUG = True
+DEBUG = False
 
 
 # noinspection DuplicatedCode
@@ -171,6 +215,8 @@ def enhance(grid: dict[Coord], key: dict[int], iteration: int, orig_size=100):
     default_fill = False
 
     # Flip the fill on empty space!
+    # if first char of the enhancement table is "on"
+    # in infinite space that will flip the whole empty stuff on
     if key[0]:
         default_fill = iteration % 2 == 0
 
@@ -178,22 +224,23 @@ def enhance(grid: dict[Coord], key: dict[int], iteration: int, orig_size=100):
     max_pos = orig_size + 2 * iteration
     for r in range(min_pos, max_pos):
         for c in range(min_pos, max_pos):
-            # binary = ""
-            index = 0
+            binary = ""
+            # index = 0  # if bitwise :-)
             for row_offset in range(-1, 2):
                 for col_offset in range(-1, 2):
                     crd = Coord(r + row_offset, c + col_offset)
-                    index = index << 1
+                    # index = index << 1  # bitwise
                     if crd in grid and grid[crd]:
-                        # binary += "1"
-                        index += 1
+                        binary += "1"
+                        # index += 1  # bitwise
                     elif crd not in grid and default_fill:
-                        # binary += "1"
-                        index += 1
-                    # else:
-                    #     binary += "0"
-            # key_index = base_x_to_10(binary, base=2)
-            ret[Coord(r, c)] = key[index]
+                        binary += "1"
+                        # index += 1  # bitwise
+                    else:  # remove if bitwise
+                        binary += "0"
+            key_index = base_x_to_10(binary, base=2)
+            # ret[Coord(r, c)] = key[index]  # bitwise
+            ret[Coord(r, c)] = key[key_index]
     return ret
 
 
@@ -203,15 +250,15 @@ def count(grid) -> int:
 
 def visualize(grid):
     g = OrderedDict(sorted(grid.items()))
+    print()
     print("-" * 100)
-    row = -1000
+    row = -1000000
     for crd in g:
         if crd.r > row:
             row = crd.r
             print()
         print("#" if g[crd] else ".", end="")
     print()
-    print("-" * 100)
 
 
 def main(source):
@@ -221,11 +268,10 @@ def main(source):
     grid_part_1 = None
     for i in range(1, 51):
         grid = enhance(grid, key, i, orig_size=orig_size)
+        if DEBUG:
+            visualize(grid)
         if i == 2:
             grid_part_1 = grid
-    if DEBUG:
-        visualize(grid_part_1)
-        visualize(grid)
 
     return count(grid_part_1), count(grid)
 
