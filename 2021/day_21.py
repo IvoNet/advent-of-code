4b900f46cc52c703d79008785b8442299f8d86e6
Ivo Woltring <ivo@ivonet.nl>
2021-12-20 12:05:15 +0100

    2021 day 21 prepare

---
+++ 2021/day_21.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+#  -*- coding: utf-8 -*-
+__author__ = "Ivo Woltring"
+__revised__ = "$revised: 01/12/2021 10:39$"
+__copyright__ = "Copyright (c) 2021 Ivo Woltring"
+__license__ = "Apache 2.0"
+__doc__ = """
+
+"""
+
+import sys
+import unittest
+from pathlib import Path
+
+from ivonet.files import read_rows
+from ivonet.iter import ints
+
+sys.dont_write_bytecode = True
+
+DEBUG = False
+
+
+# noinspection DuplicatedCode
+def _(*args, end="\n"):
+    if DEBUG:
+        print(" ".join(str(x) for x in args), end=end)
+
+
+def part_1(source):
+    return 0
+
+
+def part_2(source):
+    return 0
+
+
+class UnitTests(unittest.TestCase):
+
+    def setUp(self) -> None:
+        day = ints(Path(__file__).name)[0]
+        self.source = read_rows(f"day_{day}.txt")
+        self.test_source = read_rows("""""")
+
+    def test_example_data_part_1(self):
+        self.assertEqual(None, part_1(self.test_source))
+
+    def test_part_1(self):
+        self.assertEqual(None, part_1(self.source))
+
+    def test_example_data_part_2(self):
+        self.assertEqual(None, part_2(self.test_source))
+
+    def test_part_2(self):
+        self.assertEqual(None, part_2(self.source))
+
+
+if __name__ == '__main__':
+    unittest.main()
