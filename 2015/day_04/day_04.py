7fae6670b6836601ffb7fa1ac6726a74d1c28723
Ivo Woltring <ivo@ivonet.nl>
2021-12-21 22:43:56 +0100

    2015 day 4 done

--- 2015/day_04/day_04.py
+++ 2015/day_04/day_04.py
@@ -10,9 +10,9 @@ import sys
 import unittest
 from pathlib import Path
 
-from ivonet.files import read_rows
+from ivonet.files import read_rows, read_data
 from ivonet.iter import ints
-
+from hashlib import md5
 sys.dont_write_bytecode = True
 
 DEBUG = False
@@ -24,32 +24,41 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
-def part_1(source):
-    return 0
+def part_1(key):
+    idx = 0
+    while True:
+        answer = f"{key}{idx}".encode("ascii")
+        if md5(answer).hexdigest().startswith("00000"):
+            break
+        idx += 1
+    return idx
 
 
-def part_2(source):
-    return 0
+def part_2(key):
+    idx = 0
+    while True:
+        answer = f"{key}{idx}".encode("ascii")
+        if md5(answer).hexdigest().startswith("000000"):
+            break
+        idx += 1
+    return idx
 
 
 class UnitTests(unittest.TestCase):
 
     def setUp(self) -> None:
         day = str(ints(Path(__file__).name)[0])
-        self.source = read_rows(f"day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
+        self.source = read_data(f"day_{day.zfill(2)}.input")
+        self.test_source = read_data("""abcdef""")
 
     def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+        self.assertEqual(609043, part_1(self.test_source))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
-
-    def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(282749, part_1(self.source))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(9962624, part_2(self.source))
 
 
 if __name__ == '__main__':
