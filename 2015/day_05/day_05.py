265b43daa7cbb0a7754f2490d8723b284043635c
Ivo Woltring <ivo@ivonet.nl>
2021-12-21 23:28:20 +0100

    2015 day 5 done

--- 2015/day_05/day_05.py
+++ 2015/day_05/day_05.py
@@ -13,7 +13,7 @@ from itertools import groupby
 from pathlib import Path
 
 from ivonet.files import read_rows
-from ivonet.iter import ints
+from ivonet.iter import ints, consecutive_element_pairing
 
 sys.dont_write_bytecode = True
 
@@ -44,12 +44,34 @@ def nice(word):
     return True
 
 
+def rule_two_letters_repeated(word: str) -> bool:
+    for pair in consecutive_element_pairing(list(word), 2, lambda z: "".join([str(x) for x in z])):
+        if len(re.findall(pair, word)) >= 2:
+            return True
+    return False
+
+
+def rule_three_letters_beginning_and_end_same(word: str) -> bool:
+    for part in consecutive_element_pairing(list(word), 3, lambda z: "".join([str(x) for x in z])):
+        if part.endswith(part[0]):
+            return True
+    return False
+
+
+def nice_v2(word):
+    if not rule_two_letters_repeated(word):
+        return False
+    if not rule_three_letters_beginning_and_end_same(word):
+        return False
+    return True
+
+
 def part_1(source):
     return sum(1 for word in source if nice(word))
 
 
 def part_2(source):
-    return 0
+    return sum(1 for word in source if nice_v2(word))
 
 
 class UnitTests(unittest.TestCase):
@@ -67,13 +89,10 @@ dvszwmarrgswjxmb""")
         self.assertEqual(2, part_1(self.test_source))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
-
-    def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(258, part_1(self.source))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(53, part_2(self.source))
 
 
 if __name__ == '__main__':
