f3d9296250330ef59c105e34ff68a9b967e56d66
Ivo Woltring <ivo@ivonet.nl>
2021-12-25 22:53:37 +0100

    2015 day 6 done

--- 2015/day_06/day_06.py
+++ 2015/day_06/day_06.py
@@ -31,48 +31,69 @@ class Coord(NamedTuple):
     y: int
 
 
-def turn(grid: dict, start: Coord, stop: Coord, state=True):
+def turn_1(grid: dict, start: Coord, stop: Coord, state=True):
     for x in range(start.x, stop.x + 1):
         for y in range(start.y, stop.y + 1):
             grid[Coord(x, y)] = state
 
 
-def toggle(grid: dict, start: Coord, stop: Coord):
+def toggle_1(grid: dict, start: Coord, stop: Coord):
     for x in range(start.x, stop.x + 1):
         for y in range(start.y, stop.y + 1):
             grid[Coord(x, y)] = not grid[Coord(x, y)]
 
 
-def process(source):
+def process_1(source):
     grid = defaultdict(bool)
-    # instructions = []
     for line in source:
         x1, y1, x2, y2 = ints(line)
         _(x1, y1, x2, y2)
         start = Coord(x1, y1)
         stop = Coord(x2, y2)
         if "turn on" in line:
-            # instructions.append(("on", start, stop))
-            turn(grid, start, stop, state=True)
+            turn_1(grid, start, stop, state=True)
             continue
         if "turn off" in line:
-            # instructions.append(("off", start, stop))
-            turn(grid, start, stop, state=False)
+            turn_1(grid, start, stop, state=False)
             continue
         if "toggle" in line:
-            # instructions.append(("toggle", start, stop))
-            toggle(grid, start, stop)
-    return grid  # , instructions
+            toggle_1(grid, start, stop)
+    return grid
+
+
+def turn_2(grid: dict, start: Coord, stop: Coord, state=1):
+    for x in range(start.x, stop.x + 1):
+        for y in range(start.y, stop.y + 1):
+            grid[Coord(x, y)] += state
+            if grid[Coord(x, y)] < 0:
+                grid[Coord(x, y)] = 0
+
+
+def process_2(source):
+    grid = defaultdict(int)
+    for line in source:
+        x1, y1, x2, y2 = ints(line)
+        start = Coord(x1, y1)
+        stop = Coord(x2, y2)
+        if "turn on" in line:
+            turn_2(grid, start, stop, state=1)
+            continue
+        if "turn off" in line:
+            turn_2(grid, start, stop, state=-1)
+            continue
+        if "toggle" in line:
+            turn_2(grid, start, stop, state=2)
+    return grid
 
 
 def part_1(source):
-    grid = process(source)
+    grid = process_1(source)
     return sum(1 for v in grid.values() if v is True)
 
 
 def part_2(source):
-    # Should calculate not do!!
-    return 0
+    grid = process_2(source)
+    return sum(v for v in grid.values())
 
 
 class UnitTests(unittest.TestCase):
@@ -80,19 +101,12 @@ class UnitTests(unittest.TestCase):
     def setUp(self) -> None:
         day = str(ints(Path(__file__).name)[0])
         self.source = read_rows(f"day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
-
-    def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
 
     def test_part_1(self):
         self.assertEqual(377891, part_1(self.source))
 
-    def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
-
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(14110788, part_2(self.source))
 
 
 if __name__ == '__main__':
