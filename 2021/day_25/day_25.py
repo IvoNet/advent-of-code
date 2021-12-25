22b7c8fe5cd0c4a67579442d42bc457ea091325e
Ivo Woltring <ivo@ivonet.nl>
2021-12-25 07:43:29 +0100

    2021 day 25 Done!

--- 2021/day_25/day_25.py
+++ 2021/day_25/day_25.py
@@ -9,14 +9,19 @@ __doc__ = """"""
 
 import sys
 import unittest
+from copy import deepcopy
 from pathlib import Path
 
 from ivonet.files import read_rows
 from ivonet.iter import ints
 
+CUCUMBER_RIGHT = ">"
+OPEN_SPACE = "."
+CUCUMBER_DOWN = "v"
+
 sys.dont_write_bytecode = True
 
-DEBUG = False
+DEBUG = True
 
 
 # noinspection DuplicatedCode
@@ -25,8 +30,49 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
+def move(sea_floor, cucumber):
+    nsf = deepcopy(sea_floor)
+    has_moved = False
+    rows = len(sea_floor)
+    cols = len(sea_floor[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if cucumber != sea_floor[row][col]:
+                continue
+            if sea_floor[row][col] == CUCUMBER_DOWN:
+                r = (row + 1) % rows
+                c = col
+            else:
+                r = row
+                c = (col + 1) % cols
+
+            if sea_floor[r][c] == OPEN_SPACE:
+                nsf[r][c] = sea_floor[row][col]
+                nsf[row][col] = OPEN_SPACE
+                has_moved = True
+    return nsf, has_moved
+
+
+def visualise(sea_floor):
+    str = ""
+    for r in sea_floor:
+        str += "".join(r)
+        str += "\n"
+    return str
+
+
 def part_1(source):
-    return 0
+    sea_floor = [list(r) for r in source]
+    steps = 0
+    while steps < 10000:  # not an endless loop :-)
+        sea_floor, has_moved_right = move(sea_floor, CUCUMBER_RIGHT)
+        sea_floor, has_moved_down = move(sea_floor, CUCUMBER_DOWN)
+        steps += 1
+        if not (has_moved_right or has_moved_down):
+            _(visualise(sea_floor))
+            return steps
+    return -1
 
 
 def part_2(source):
@@ -38,19 +84,21 @@ class UnitTests(unittest.TestCase):
     def setUp(self) -> None:
         day = str(ints(Path(__file__).name)[0])
         self.source = read_rows(f"day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
+        self.test_source = read_rows("""v...>>.vv>
+.vv>>.vv..
+>>.>v>...v
+>>v>>.>.v.
+v>v.vv.v..
+>.>>..v...
+.vv..>.>v.
+v.v..>>v.v
+....v..v.>""")
 
     def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+        self.assertEqual(58, part_1(self.test_source))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
-
-    def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
-
-    def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(509, part_1(self.source))
 
 
 if __name__ == '__main__':
