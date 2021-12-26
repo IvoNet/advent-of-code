549393128b845c3f51000a17e0cc4a9faaf1ebe6
Ivo Woltring <ivo@ivonet.nl>
2021-12-26 23:04:00 +0100

    2015 day 8 done

--- 2015/day_08/day_08.py
+++ 2015/day_08/day_08.py
@@ -25,11 +25,23 @@ def _(*args, end="\n"):
 
 
 def part_1(source):
-    return 0
+    code = 0
+    memory = 0
+    for line in source:
+        code += len(line)
+        memory += len(eval(line))
+    return code - memory
 
 
 def part_2(source):
-    return 0
+    orig = 0
+    new_code = 0
+    for line in source:
+        orig += len(line)
+        nl = '"' + line.replace("\\", "\\\\").replace('"','\\"') + '"'
+        _(nl)
+        new_code += len(nl)
+    return new_code - orig
 
 
 class UnitTests(unittest.TestCase):
@@ -37,19 +49,13 @@ class UnitTests(unittest.TestCase):
     def setUp(self) -> None:
         day = str(ints(Path(__file__).name)[0])
         self.source = read_rows(f"day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
-
-    def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
-
-    def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
+        self.test_source = read_rows(f"day_{day.zfill(2)}_test.input")
 
     def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(19, part_2(self.test_source))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(2046, part_2(self.source))
 
 
 if __name__ == '__main__':
