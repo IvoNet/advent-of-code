6495e1c502c24f834416f52b2ef5827cf9943984
Ivo Woltring <ivo@ivonet.nl>
2021-12-27 21:46:40 +0100

    2015 day 16 part 1

--- 2015/day_16/day_16.py
+++ 2015/day_16/day_16.py
@@ -15,7 +15,7 @@ from ivonet.iter import ints
 
 sys.dont_write_bytecode = True
 
-DEBUG = False
+DEBUG = True
 
 
 # noinspection DuplicatedCode
@@ -24,8 +24,46 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
-def part_1(source):
-    return 0
+def logic(logica):
+    ret = {}
+    for line in logica:
+        key = line.split(":")[0].strip()
+        value = ints(line)[0]
+        ret[key] = value
+    return ret
+
+
+def aunts(source):
+    ret = {}
+    for line in source:
+        line = line.replace(":", "").replace(",", "")
+        nrs = ints(line)
+        assert len(nrs) == 4
+        items = line.split()
+        ret[nrs[0]] = {items[2]: nrs[1],
+                       items[4]: nrs[2],
+                       items[6]: nrs[3]}
+    return ret
+
+
+def narrow_down(aunties: dict, item: str, amount: int):
+    ret = aunties.copy()
+    for aunt, items in aunties.items():
+        if item in items and aunties[aunt][item] != amount:
+            del ret[aunt]
+    return ret
+
+
+def part_1(source, logica):
+    facts = logic(logica)
+    aunties = aunts(source)
+    _(facts)
+    _(aunties)
+    for key, value in facts.items():
+        aunties = narrow_down(aunties, key, value)
+        _(aunties)
+    assert len(aunties) == 1
+    return list(aunties.keys())[0]
 
 
 def part_2(source):
@@ -37,16 +75,19 @@ class UnitTests(unittest.TestCase):
     def setUp(self) -> None:
         day = str(ints(Path(__file__).name)[0])
         self.source = read_rows(f"day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
-
-    def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+        self.logica = read_rows("""children: 3
+cats: 7
+samoyeds: 2
+pomeranians: 3
+akitas: 0
+vizslas: 0
+goldfish: 5
+trees: 3
+cars: 2
+perfumes: 1""")
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
-
-    def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(373, part_1(self.source, self.logica))
 
     def test_part_2(self):
         self.assertEqual(None, part_2(self.source))
