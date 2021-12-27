378593f4556f5ddc9beb3f97cdbb666525c34dd0
Ivo Woltring <ivo@ivonet.nl>
2021-12-27 22:35:49 +0100

    2015 day 17 done

--- 2015/day_17/day_17.py
+++ 2015/day_17/day_17.py
@@ -8,15 +8,15 @@ __license__ = "Apache 2.0"
 
 import sys
 import unittest
-from itertools import permutations, combinations
+from itertools import combinations
 from pathlib import Path
 
-from ivonet.files import read_rows, read_ints
+from ivonet.files import read_ints
 from ivonet.iter import ints
 
 sys.dont_write_bytecode = True
 
-DEBUG = True
+DEBUG = False
 
 
 # noinspection DuplicatedCode
@@ -25,18 +25,28 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
-def part_1(source, liters=150):
+def containers(liters, source, shortest=False):
     count = 0
+    combi = []
     for i in range(2, len(source)):
+        if shortest and combi:
+            break
         for x in combinations(source, i):
             if sum(x) == liters:
-                _(x, 150)
+                _(x, "->", 150)
+                combi.append(x)
                 count += 1
+    return combi, count
+
+
+def part_1(source, liters=150):
+    combi, count = containers(liters, source)
     return count
 
 
-def part_2(source):
-    return 0
+def part_2(source, liters=150):
+    combi, count = containers(liters, source, shortest=True)
+    return count
 
 
 class UnitTests(unittest.TestCase):
@@ -54,13 +64,10 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(4, part_1(self.test_source, liters=25))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
-
-    def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(1304, part_1(self.source))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(18, part_2(self.source))
 
 
 if __name__ == '__main__':
