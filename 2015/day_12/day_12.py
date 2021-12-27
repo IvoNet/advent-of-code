7801a4e1e25ff65262e63cf90372e0e8294b03e3
Ivo Woltring <ivo@ivonet.nl>
2021-12-27 16:16:10 +0100

    2015 day 12 done

--- 2015/day_12/day_12.py
+++ 2015/day_12/day_12.py
@@ -6,11 +6,12 @@ __author__ = "Ivo Woltring"
 __copyright__ = "Copyright (c) 2021 Ivo Woltring"
 __license__ = "Apache 2.0"
 
+import json
 import sys
 import unittest
 from pathlib import Path
 
-from ivonet.files import read_rows
+from ivonet.files import read_data
 from ivonet.iter import ints
 
 sys.dont_write_bytecode = True
@@ -24,32 +25,81 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
+def remove_red(source):
+    """First try of part 2.... went wrong!"""
+    try:
+        idx = source.index("red")
+    except ValueError:
+        return source
+    start = -1
+    pos = -1
+    for i in range(idx, -1, -1):
+        if source[i] == "[":
+            break
+        if source[i] == "{":
+            start = 1
+            pos = i
+            break
+    if start >= 0:
+        if pos >= 0:
+            for i in range(idx, len(source)):
+                if source[i] == "{":
+                    start += 1
+                elif source[i] == "}":
+                    start -= 1
+                if start == 0:
+                    new = source[:pos] + source[i + 1:]
+                    return remove_red(new)
+    elif idx >= 0:
+        return remove_red(source.replace("red", "!!!", 1))
+    _(source)
+    return source
+
+
+def exclude_red_sum(source):
+    if isinstance(source, int):
+        return source
+    elif isinstance(source, str):
+        return 0
+    elif isinstance(source, list):
+        return sum(exclude_red_sum(x) for x in source)
+    else:  # dict
+        if any(x == "red" for x in source.values()):
+            return 0
+        else:
+            return sum(exclude_red_sum(x) for x in source.values())
+
+
 def part_1(source):
-    return 0
+    """Don't treat it as a Python or JSON object but just do regex on all integers and sum them"""
+    return sum(ints(source))
 
 
 def part_2(source):
-    return 0
+    data = json.loads(source)
+    return exclude_red_sum(data)
 
 
 class UnitTests(unittest.TestCase):
 
     def setUp(self) -> None:
         day = str(ints(Path(__file__).name)[0])
-        self.source = read_rows(f"day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
+        self.source = read_data(f"day_{day.zfill(2)}.input")
+        self.test_source = read_data("""[1,2,3]""")
 
     def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+        self.assertEqual(6, part_1(self.test_source))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
+        self.assertEqual(111754, part_1(self.source))
 
     def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(4, part_2('[1,{"c":"red","b":2},3]'))
+        self.assertEqual(0, part_2('{"d":"red","e":[1,2,3,4],"f":5}'))
+        self.assertEqual(6, part_2('[1,"red",5]'))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(65402, part_2(self.source))
 
 
 if __name__ == '__main__':
