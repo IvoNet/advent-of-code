72a345ec353d8af2a7815ec6556b2a53ddd8c1e5
Ivo Woltring <ivo@ivonet.nl>
2021-12-31 00:34:29 +0100

    2015 day 25 done 50 stars!

--- 2015/day_25/day_25.py
+++ 2015/day_25/day_25.py
@@ -10,11 +10,12 @@ import sys
 import unittest
 from pathlib import Path
 
-from ivonet.files import read_rows
+from ivonet.files import read_data
 from ivonet.iter import ints
 
 sys.dont_write_bytecode = True
 
+START = 20151125
 DEBUG = False
 
 
@@ -24,32 +25,46 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
+def triangle_number(col: int) -> int:
+    """https://en.wikipedia.org/wiki/Triangular_number"""
+    return int((col * (col + 1)) / 2)
+
+
+def lazy_caterer(row: int) -> int:
+    """https://en.wikipedia.org/wiki/Lazy_caterer%27s_sequence"""
+    return 1 + triangle_number(row - 1)
+
+
+def code_gen():
+    ret = START
+    while True:
+        yield ret
+        ret = (ret * 252533) % 33554393
+
+
+def value(row, col):
+    """Get the laze"""
+    it = lazy_caterer(row)
+    for i in range(1, col):
+        it += row + i
+    for i, code in enumerate(code_gen()):
+        if i == it - 1:
+            return code
+
+
 def part_1(source):
-    return 0
-
-
-def part_2(source):
-    return 0
+    row, col = ints(source)
+    return value(row, col)
 
 
 class UnitTests(unittest.TestCase):
 
     def setUp(self) -> None:
         day = str(ints(Path(__file__).name)[0])
-        self.source = read_rows(f"day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
-
-    def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+        self.source = read_data(f"day_{day.zfill(2)}.input")
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
-
-    def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
-
-    def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(8997277, part_1(self.source))
 
 
 if __name__ == '__main__':
