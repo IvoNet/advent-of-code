ce5a7f63d56d67674e7462f5e5f298a902fcf86f
Ivo Woltring <ivo@ivonet.nl>
2021-12-30 20:01:54 +0100

    2015 day 23 done

--- 2015/day_23/day_23.py
+++ 2015/day_23/day_23.py
@@ -24,12 +24,63 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
-def part_1(source):
-    return 0
+def prepare(source):
+    program = []
+    for line in source:
+        cmd = line.replace(",", "").strip().split()
+        if cmd[0] in ("jio", "jmp", "jie"):
+            del (cmd[-1])
+            cmd.append(ints(line)[0])
+        program.append(cmd)
+    return program
+
+
+def part_1(source, a=0, b=0):
+    program = prepare(source)
+    register = {
+        "a": a,
+        "b": b
+    }
+    i = 0
+    while True:
+        try:
+            cmd = program[i]
+        except IndexError:
+            return register["b"]
+        if cmd[0] == "inc":
+            register[cmd[1]] += 1
+            _(f"inc {cmd[1]} to {register[cmd[1]]}")
+            i += 1
+            continue
+        if cmd[0] == "tpl":
+            register[cmd[1]] *= 3
+            _(f"tpl {cmd[1]} to {register[cmd[1]]}")
+            i += 1
+            continue
+        if cmd[0] == "hlf":
+            register[cmd[1]] //= 2
+            _(f"hlf {cmd[1]} to {register[cmd[1]]}")
+            i += 1
+            continue
+        if cmd[0] == "jmp":
+            i += cmd[1]
+            _(f"jmp to cmd {i}")
+            continue
+        if cmd[0] == "jio" and register[cmd[1]] == 1:
+            i += cmd[2]
+            _(f"jio {cmd[1]} to {i}")
+            continue
+        if cmd[0] == "jie" and register[cmd[1]] % 2 == 0:
+            i += cmd[2]
+            _(f"jie {cmd[1]} to {i}")
+            continue
+        _("No command going to next command:", cmd)
+        i += 1
+    return None
 
 
 def part_2(source):
-    return 0
+    return part_1(source, a=1)
 
 
 class UnitTests(unittest.TestCase):
@@ -37,19 +88,12 @@ class UnitTests(unittest.TestCase):
     def setUp(self) -> None:
         day = str(ints(Path(__file__).name)[0])
         self.source = read_rows(f"day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
-
-    def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
-
-    def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(170, part_1(self.source))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(247, part_2(self.source))
 
 
 if __name__ == '__main__':
