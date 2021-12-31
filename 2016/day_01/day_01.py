82e2fa868ab237fb8cf6906182703017aa718d2f
Ivo Woltring <ivo@ivonet.nl>
2021-12-31 14:35:55 +0100

    2016 day 01 WIP

--- 2016/day_01/day_01.py
+++ 2016/day_01/day_01.py
@@ -9,14 +9,16 @@ __doc__ = """"""
 
 import sys
 import unittest
+from collections import defaultdict
 from pathlib import Path
+from typing import NamedTuple
 
-from ivonet.files import read_rows
+from ivonet.files import read_data
 from ivonet.iter import ints
 
 sys.dont_write_bytecode = True
 
-DEBUG = False
+DEBUG = True
 
 
 # noinspection DuplicatedCode
@@ -25,8 +27,77 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
+def manhattan_distance(goal: Location) -> Callable[[Location], float]:
+    def distance(ml: Location) -> float:
+        xdist: int = abs(ml.col - goal.col)
+        ydist: int = abs(ml.row - goal.row)
+        return xdist + ydist
+
+    return distance
+
+
+class Location(NamedTuple):
+    row: int
+    col: int
+
+
+class Instruction(NamedTuple):
+    direction: str
+    blocks: int
+
+
+def parse(source: str) -> list[Instruction]:
+    return [Instruction(x[0], int(x[1])) for x in source.strip().split(", ")]
+
+
+class City:
+
+    def __init__(self, instructions: list[Instruction], start: Location = Location(0, 0),
+                 start_orientation="N") -> None:
+        self.instructions = instructions
+        self.start = start
+        self.orientation = start_orientation
+        self.directions = {
+            "N": {
+                "L": "W",
+                "R": "E"
+            },
+            "E": {
+                "L": "N",
+                "R": "S"
+            },
+            "S": {
+                "L": "E",
+                "R": "W"
+            },
+            "W": {
+                "L": "S",
+                "R": "N"
+            },
+        }
+        self.location = defaultdict(int)
+        self.distance = manhattan_distance(self.start)
+
+    def go(self, direction, blocks):
+        self.orientation = self.directions[self.orientation][direction]
+        self.location[self.orientation] += blocks
+
+    def current_location(self) -> Location:
+        up = abs(self.location["N"] - self.location["S"])
+        right = abs(self.location["W"] - self.location["E"])
+        _(up, right)
+        return Location(up, right)
+
+    def walk(self):
+        for instruction in self.instructions:
+            self.go(*instruction)
+            _(self.location)
+        return self.distance(self.current_location())
+
+
 def part_1(source):
-    return 0
+    instructions = parse(source)
+    return City(instructions).walk()
 
 
 def part_2(source):
@@ -37,11 +108,15 @@ class UnitTests(unittest.TestCase):
 
     def setUp(self) -> None:
         day = str(ints(Path(__file__).name)[0])
-        self.source = read_rows(f"day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
+        self.source = read_data(f"day_{day.zfill(2)}.input")
+        self.test_source = read_data("""R5, L5, R5, R3""")
+        self.test_source2 = read_data("""R2, R2, R2""")
 
     def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+        self.assertEqual(12, part_1(self.test_source))
+
+    def test_example_data_1_part_1(self):
+        self.assertEqual(2, part_1(self.test_source2))
 
     def test_part_1(self):
         self.assertEqual(None, part_1(self.source))
