d8fc3851455419136728c754568d4042e24c9c86
Ivo Woltring <ivo@ivonet.nl>
2022-01-01 11:52:54 +0100

    2016 day 2 done

--- 2016/day_02/day_02.py
+++ 2016/day_02/day_02.py
@@ -10,7 +10,6 @@ import sys
 import unittest
 from dataclasses import dataclass
 from pathlib import Path
-from typing import NamedTuple
 
 from ivonet.files import read_rows
 from ivonet.iter import ints
@@ -26,9 +25,8 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
-
 @dataclass
-class Location:
+class StandardKeypad:
     keypad = [
         [1, 2, 3],
         [4, 5, 6],
@@ -66,19 +64,74 @@ class Location:
             raise ValueError("Should not be here")
 
 
+@dataclass
+class DesignKeypad:
+    keypad = [
+        [None, None, "1", None, None],
+        [None, "2", "3", "4", None],
+        ["5", "6", "7", "8", "9"],
+        [None, "A", "B", "C", None],
+        [None, None, "D", None, None]
+
+    ]
+
+    row: int
+    col: int
+
+    def up(self):
+        row = self.row - 1
+        if row >= 0 and self.keypad[row][self.col] is not None:
+            self.row = row
+
+    def down(self):
+        row = self.row + 1
+        if row < len(self.keypad) and self.keypad[row][self.col] is not None:
+            self.row = row
+
+    def left(self):
+        col = self.col - 1
+        if col >= 0 and self.keypad[self.row][col] is not None:
+            self.col = col
+
+    def right(self):
+        col = self.col + 1
+        if col < len(self.keypad[0]) and self.keypad[self.row][col] is not None:
+            self.col = col
+
+    def key(self):
+        return self.keypad[self.row][self.col]
+
+    def move(self, m):
+        if m == "L":
+            self.left()
+        elif m == "R":
+            self.right()
+        elif m == "U":
+            self.up()
+        elif m == "D":
+            self.down()
+        else:
+            raise ValueError("Should not be here")
+
 
 def part_1(source):
-    loc = Location(1,1)
+    loc = StandardKeypad(1, 1)
     nr = ""
     for line in source:
         for c in line:
             loc.move(c)
         nr += str(loc.key())
-    return int(nr)
+    return nr
 
 
 def part_2(source):
-    return 0
+    loc = DesignKeypad(2, 0)
+    nr = ""
+    for line in source:
+        for c in line:
+            loc.move(c)
+        nr += loc.key()
+    return nr
 
 
 class UnitTests(unittest.TestCase):
@@ -92,13 +145,13 @@ LURDL
 UUUUD""")
 
     def test_example_data_part_1(self):
-        self.assertEqual(1985, part_1(self.test_source))
+        self.assertEqual("1985", part_1(self.test_source))
 
     def test_part_1(self):
-        self.assertEqual(38961, part_1(self.source))
+        self.assertEqual("38961", part_1(self.source))
 
     def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual("5DB3", part_2(self.test_source))
 
     def test_part_2(self):
         self.assertEqual(None, part_2(self.source))
