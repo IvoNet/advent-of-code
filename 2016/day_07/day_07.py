50278dfc738e55b6df91c973a5d59966186d5f39
Ivo Woltring <ivo@ivonet.nl>
2022-01-02 00:49:18 +0100

    2016 day 7 part 1

--- 2016/day_07/day_07.py
+++ 2016/day_07/day_07.py
@@ -7,12 +7,13 @@ __copyright__ = "Copyright (c) 2021 Ivo Woltring"
 __license__ = "Apache 2.0"
 __doc__ = """"""
 
+import re
 import sys
 import unittest
 from pathlib import Path
 
 from ivonet.files import read_rows
-from ivonet.iter import ints
+from ivonet.iter import ints, consecutive_element_pairing
 
 sys.dont_write_bytecode = True
 
@@ -25,8 +26,34 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
+class Ip:
+
+    def __init__(self, ip) -> None:
+        self.parts = re.findall(r"(\w+)", ip)
+        self.brackets = re.findall(r"\[(\w+)]", ip)
+        for b in self.brackets:
+            self.parts.remove(b)
+        _(self.parts, self.brackets)
+        assert self.parts is not None
+        assert self.brackets is not None
+
+    def has_tls(self):
+        for word in self.brackets:
+            for a, b, c, d in consecutive_element_pairing(word, 4, list):
+                if a != b and c != d and a == d and b == c:
+                    return False
+        for word in self.parts:
+            for a, b, c, d in consecutive_element_pairing(word, 4, list):
+                if a != b and c != d and a == d and b == c:
+                    return True
+        return False
+
+
 def part_1(source):
-    return 0
+    total = 0
+    for ip in source:
+        total += 1 if Ip(ip).has_tls() else 0
+    return total
 
 
 def part_2(source):
@@ -39,13 +66,21 @@ class UnitTests(unittest.TestCase):
         print()
         day = str(ints(Path(__file__).name)[0])
         self.source = read_rows(f"day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
+        self.test_source = read_rows("""abba[mnop]qrst""")
+        self.test_source_wrong_1 = read_rows("""abcd[bddb]xyyx""")
+        self.test_source_wrong_2 = read_rows("""aaaa[qwer]tyui""")
 
-    def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+    def test_example_1_data_part_1(self):
+        self.assertEqual(1, part_1(self.test_source))
+
+    def test_example_2_data_part_1(self):
+        self.assertEqual(0, part_1(self.test_source_wrong_1))
+
+    def test_example_3_data_part_1(self):
+        self.assertEqual(0, part_1(self.test_source_wrong_2))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
+        self.assertEqual(115, part_1(self.source))
 
     def test_example_data_part_2(self):
         self.assertEqual(None, part_2(self.test_source))
