0451aba4d7117d27651d9a97ea0e28e5ef7e0a99
Ivo Woltring <ivo@ivonet.nl>
2022-01-01 23:56:21 +0100

    2016 day 6 Done

--- 2016/day_06/day_06.py
+++ 2016/day_06/day_06.py
@@ -5,9 +5,11 @@ from __future__ import annotations
 __author__ = "Ivo Woltring"
 __copyright__ = "Copyright (c) 2021 Ivo Woltring"
 __license__ = "Apache 2.0"
+__doc__ = """"""
 
 import sys
 import unittest
+from collections import Counter
 from pathlib import Path
 
 from ivonet.files import read_rows
@@ -25,31 +27,56 @@ def _(*args, end="\n"):
 
 
 def part_1(source):
-    return 0
+    data = [list(x) for x in source]
+    by_col = list(zip(*data))
+    _(by_col)
+    msg = "".join([Counter(x).most_common(1)[0][0] for x in by_col])
+    _(msg)
+    return msg
 
 
 def part_2(source):
-    return 0
+    data = [list(x) for x in source]
+    by_col = list(zip(*data))
+    msg = "".join([Counter(x).most_common()[-1][0] for x in by_col])
+    _(msg)
+    return msg
 
 
 class UnitTests(unittest.TestCase):
 
     def setUp(self) -> None:
+        print()
         day = str(ints(Path(__file__).name)[0])
         self.source = read_rows(f"day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
+        self.test_source = read_rows("""eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar""")
 
     def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+        self.assertEqual("easter", part_1(self.test_source))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
+        self.assertEqual("ygjzvzib", part_1(self.source))
 
     def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual("advent", part_2(self.test_source))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual("pdesmnoz", part_2(self.source))
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -55,7 +55,7 @@ virtual environment.
 - None yet
 
 # 2016
-- days 1..5
+- days 1..6
 
 ## 2015
 - 50 stars!
