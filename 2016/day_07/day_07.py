5173cd814ccde8e0fa5bf16bcecc19d430f130d0
Ivo Woltring <ivo@ivonet.nl>
2022-01-02 01:12:04 +0100

    2016 day 7 - refactoring

--- 2016/day_07/day_07.py
+++ 2016/day_07/day_07.py
@@ -27,17 +27,22 @@ def _(*args, end="\n"):
 
 
 class Ip:
+    """Representation of IPv7"""
 
     def __init__(self, ip) -> None:
-        self.parts = re.findall(r"(\w+)", ip)
+        self.parts = re.findall(r"(\w+)", ip)  # finds all of them
         self.brackets = re.findall(r"\[(\w+)]", ip)
         for b in self.brackets:
-            self.parts.remove(b)
-        _(self.parts, self.brackets)
+            self.parts.remove(b)  # just remove them from the parts list
         assert self.parts is not None
         assert self.brackets is not None
+        _(self.parts, self.brackets)
 
     def has_tls(self):
+        """TLS (transport-layer snooping) protocol checker
+        - if ABBA exists in the bracketed words it is not good
+        - if not rule one and ABBA exists in the parts then good
+        """
         for word in self.brackets:
             for a, b, c, d in consecutive_element_pairing(word, 4, list):
                 if a != b and c != d and a == d and b == c:
@@ -49,6 +54,10 @@ class Ip:
         return False
 
     def has_ssl(self):
+        """SSL (super-secret listening) protocol checker
+        if any ABA combi exists in the supernet sequences and has a corresponding
+        BAB sequence in the hypernet sequences all is good otherwise not.
+        """
         aba = []
         for word in self.parts:
             for a, b, c in consecutive_element_pairing(word, 3, list):
@@ -64,17 +73,11 @@ class Ip:
 
 
 def part_1(source):
-    total = 0
-    for ip in source:
-        total += 1 if Ip(ip).has_tls() else 0
-    return total
+    return sum(1 for x in source if Ip(x).has_tls())
 
 
 def part_2(source):
-    total = 0
-    for ip in source:
-        total += 1 if Ip(ip).has_ssl() else 0
-    return total
+    return sum(1 for x in source if Ip(x).has_ssl())
 
 
 class UnitTests(unittest.TestCase):
@@ -84,17 +87,21 @@ class UnitTests(unittest.TestCase):
         day = str(ints(Path(__file__).name)[0])
         self.source = read_rows(f"day_{day.zfill(2)}.input")
         self.test_source = read_rows("""abba[mnop]qrst""")
-        self.test_source_wrong_1 = read_rows("""abcd[bddb]xyyx""")
-        self.test_source_wrong_2 = read_rows("""aaaa[qwer]tyui""")
+
+    def test_tls_1(self):
+        self.assertTrue(Ip("abba[mnop]qrst").has_tls())
+
+    def test_tls_2(self):
+        self.assertFalse(Ip("abcd[bddb]xyyx").has_tls())
+
+    def test_tls_3(self):
+        self.assertFalse(Ip("aaaa[qwer]tyui").has_tls())
+
+    def test_tls_4(self):
+        self.assertTrue(Ip("ioxxoj[asdfgh]zxcvbn").has_tls())
 
     def test_example_1_data_part_1(self):
         self.assertEqual(1, part_1(self.test_source))
-
-    def test_example_2_data_part_1(self):
-        self.assertEqual(0, part_1(self.test_source_wrong_1))
-
-    def test_example_3_data_part_1(self):
-        self.assertEqual(0, part_1(self.test_source_wrong_2))
 
     def test_part_1(self):
         self.assertEqual(115, part_1(self.source))
