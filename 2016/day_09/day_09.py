752684e6957d9e02fd8e42c41baf8e442bc06ba2
Ivo Woltring <ivo@ivonet.nl>
2022-01-03 17:49:52 +0100

    2016 day 09 part 2 wip

--- 2016/day_09/day_09.py
+++ 2016/day_09/day_09.py
@@ -8,11 +8,12 @@ __license__ = "Apache 2.0"
 __doc__ = """"""
 
 import os
+import re
 import sys
 import unittest
 from pathlib import Path
 
-from ivonet.files import read_rows
+from ivonet.files import read_data
 from ivonet.iter import ints
 
 sys.dont_write_bytecode = True
@@ -26,8 +27,60 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
+class Decoder:
+
+    def __init__(self, source) -> None:
+        self.source = source
+        self.result = ""
+        self.parse()
+
+    def parse(self):
+        while True:
+            match = re.search("\(([0-9]+)x([0-9]+)\)", self.source)
+            if not match:
+                self.result += self.source
+                break
+            b, e = match.span()
+            l, t = [int(x) for x in match.groups()]
+            self.result += self.source[:b]
+            self.result += self.source[e:e + l] * t
+            self.source = self.source[e + l:]
+
+    def __str__(self) -> str:
+        return self.result
+
+
+class Decompressor:
+
+    def __init__(self, source) -> None:
+        self.source = source
+        self.result = ""
+        self.parse()
+
+    def parse(self):
+        while True:
+            match = re.search("\)([0-9]+)x([0-9]+)\(", self.source)
+            _(match)
+            if not match:
+                self.source = self.source[::-1]
+                break
+            b, e = match.span()
+            t, l = [int(x) for x in match.groups()]
+            result = self.source[:b - l]
+            _(result)
+            result += self.source[b - l:b] * t
+            _(result)
+            self.source = result + self.source[e:]
+            _(self.source)
+
+    def __str__(self) -> str:
+        return self.source
+
+
 def part_1(source):
-    return None
+    d = Decoder(source)
+    _(d)
+    return len(d.source)
 
 
 def part_2(source):
@@ -40,17 +93,22 @@ class UnitTests(unittest.TestCase):
         if DEBUG:
             print()
         day = str(ints(Path(__file__).name)[0])
-        self.source = read_rows(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
+        self.source = read_data(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")
 
-    def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+    def test_example_1_part_1(self):
+        self.assertEqual("ABBBBBC", Decoder("A(1x5)BC").result)
+
+    def test_example_2_part_1(self):
+        self.assertEqual("XYZXYZXYZ", Decoder("(3x3)XYZ").result)
+
+    def test_example_3_part_1(self):
+        self.assertEqual("ABCBCDEFEFG", Decoder("A(2x2)BCD(2x2)EFG").result)
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
+        self.assertEqual(102239, part_1(self.source))
 
     def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual("XABCABCABCABCABCABCY", Decompressor("X(8x2)(3x3)ABCY").source)
 
     def test_part_2(self):
         self.assertEqual(None, part_2(self.source))
