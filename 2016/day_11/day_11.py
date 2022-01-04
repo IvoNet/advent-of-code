72f820ad170b15303e2c998f74bac14356e891e6
Ivo Woltring <ivo@ivonet.nl>
2022-01-04 19:10:10 +0100

    2016 day 11 wip 3

--- 2016/day_11/day_11.py
+++ 2016/day_11/day_11.py
@@ -7,11 +7,16 @@ __copyright__ = "Copyright (c) 2021 Ivo Woltring"
 __license__ = "Apache 2.0"
 __doc__ = """"""
 
+import os
+import re
 import sys
 import unittest
 from copy import deepcopy
 from itertools import combinations
+from pathlib import Path
 
+from ivonet.files import read_rows
+from ivonet.iter import ints
 from ivonet.search import bfs, node_to_path
 
 sys.dont_write_bytecode = True
@@ -69,36 +74,42 @@ def display_solution(path: List[State]):
 
 
 class Item(object):
-    def __init__(self, element): self.element = element
+    def __init__(self, element):
+        self.element = element
 
-    def __repr__(self): return '{}<{}>'.format(self.__class__.__name__, self.element)
+    def __repr__(self):
+        return '{}<{}>'.format(self.__class__.__name__, self.element)
 
-    def __hash__(self): return hash(repr(self))
+    def __hash__(self):
+        return hash(repr(self))
 
-    def __eq__(self, other): return repr(self) == repr(other)
+    def __eq__(self, other):
+        return repr(self) == repr(other)
 
-    def __lt__(self, other): return repr(self) < repr(other)
+    def __lt__(self, other):
+        return repr(self) < repr(other)
 
 
-class Generator(Thing):
+class Generator(Item):
     ...
 
 
-class Microchip(Thing):
+class Microchip(Item):
     ...
 
 
 class State(object):
 
-    def __init__(self, floorplan: dict[[int], list[str]], elevator: int = 1) -> None:
-        self.floorplan: dict[[int], list[str]] = deepcopy(floorplan)
+    def __init__(self, floorplan: list[set], elevator: int = 0) -> None:
+        self.floorplan: list[set] = deepcopy(floorplan)
         self.elevator: int = elevator
+        self._hash = hash(repr(self))
 
     def goal_test(self) -> bool:
         """The goal has been reached when:
         - all generators and their microchips are on the forth floor
         """
-        return len(self.floorplan[4]) == 10 and self.elevator == 4 and self.is_legal
+        return not any(self.floorplan[:-1])
 
     def has_generators(self, floor) -> bool:
         return len([x for x in self.floorplan[floor] if x.endswith("G")]) > 0
@@ -109,63 +120,87 @@ class State(object):
     @property
     def is_legal(self) -> bool:
         """Rulez
-        - if xM on floor then xG must be there or no other RTG
-        - chips do not effect each other if there are no RTG's
-        - you can only move from the floor with the elevator
+        A state is not legal when:
+        - a microchip is on a floor with a generator not its own
         """
-        if self.elevator < 1:
+        if self.elevator < 0:
             return False
-        if self.elevator > 4:
+        if self.elevator > 3:
             return False
         for floor in self.floorplan:
-            for chip in [x for x in self.floorplan[floor] if x.endswith("M")]:
-                if not self.has_correct_generator(chip, floor) and self.has_generators(floor):
-                    return False
+            generators = {x for x in floor if isinstance(x, Generator)}
+            if not generators:
+                continue
+            chips = {x for x in floor if isinstance(x, Microchip)}
+            if any(chip for chip in chips if not Generator(chip.element) in generators):
+                return False
         return True
 
     def successors(self) -> list[State]:
         """create all new states from current state"""
         sucs: list[State] = []
-        for i in [1, 2]:
-            for item in [list(x) for x in combinations(self.floorplan[self.elevator], i)]:
-                _(item)
-                fp = deepcopy(self.floorplan)
-                [fp[self.elevator].remove(x) for x in item]
-                if self.elevator + 1 <= 4:
-                    fp_up = deepcopy(fp)
-                    fp_up[self.elevator + 1].extend(item)
-                    sucs.append(State(fp_up, self.elevator + 1))
-                if self.elevator - 1 >= 1:
-                    fp_down = deepcopy(fp)
-                    fp_down[self.elevator - 1].extend(item)
-                    sucs.append(State(fp_down, self.elevator - 1))
-        # return [x for x in sucs if x.is_legal]
-        return list(set([x for x in sucs if x.is_legal]))
+        for delta in [-1, 1]:
+            for count in [1, 2]:
+                for items in [list(x) for x in combinations(self.floorplan[self.elevator], count)]:
+                    _(items)
+                    if not (0 <= self.elevator + delta < len(self.floorplan)):
+                        continue
+                    new_floor_plan = deepcopy(self.floorplan)
+                    for item in items:
+                        _(self, items)
+                        new_floor_plan[self.elevator].remove(item)
+                        new_floor_plan[self.elevator + delta].add(item)
+                    sucs.append(State(new_floor_plan, elevator=self.elevator + delta))
+        return [x for x in sucs if x.is_legal]
 
     def __str__(self) -> str:
-        ret = ""
-        for floor in self.floorplan:
-            ret += f"Floor {floor} has {sorted(self.floorplan[floor])}.\n"
-        ret += f"The elevator is on floor {self.elevator}.\n"
-        return ret
+        ret = []
+        for i, floor in enumerate(self.floorplan):
+            level = ('[{}]' if self.elevator == i else ' {} ').format(i + 1)
+            items = ' '.join(str(item) for item in sorted(floor))
+            ret.append(f"{level} {items}")
+        return "\n".join(reversed(ret))
 
     def __repr__(self) -> str:
-        return self.__str__()
+        ret = []
+        for items in self.floorplan:
+            if items:
+                ret.append(" ".join(repr(item) for item in sorted(items)))
+            else:
+                ret.append("-")
+        return f"State<{self.elevator} {', '.join(ret)}>"
 
     def __eq__(self, other):
         return hash(self) == hash(other)
 
     def __hash__(self):
-        return hash(repr(self))
+        return self._hash
 
 
-def part_1():
-    start = State(PART_1_FLOOR_PLAN, elevator=1)
+def parse(source):
+    re_object = re.compile(r'(\w+)(?:-compatible)? (generator|microchip)')
+    floors = []
+    for line in source:
+        if not line.strip():
+            continue
+        floor = set()
+        for element, type in re_object.findall(line):
+            if type == 'generator':
+                floor.add(Generator(element))
+            elif type == 'microchip':
+                floor.add(Microchip(element))
+        floors.append(floor)
+    return State(floors)
+
+
+def part_1(source):
+    start = parse(source)
+    _(start)
     solution = bfs(start, State.goal_test, State.successors)
     return len(node_to_path(solution))
 
 
-def part_2():
+def part_2(source):
     return None
 
 
@@ -174,6 +209,8 @@ class UnitTests(unittest.TestCase):
     def setUp(self) -> None:
         if DEBUG:
             print()
+        day = str(ints(Path(__file__).name)[0])
+        self.source = read_rows(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")
 
     def test_state_1(self):
         floor_plan = {
@@ -252,10 +289,10 @@ class UnitTests(unittest.TestCase):
         self.assertEquals(26, len(successors))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1())
+        self.assertEqual(None, part_1(self.source))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2())
+        self.assertEqual(None, part_2(self.source))
 
 
 if __name__ == '__main__':
