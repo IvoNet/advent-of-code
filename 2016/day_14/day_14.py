30ef542cb149fff9fe9cf5358aa3884c8a041af9
Ivo Woltring <ivo@ivonet.nl>
2022-01-07 17:31:27 +0100

    2016 day 14 done

--- 2016/day_14/day_14.py
+++ 2016/day_14/day_14.py
@@ -10,6 +10,7 @@ __doc__ = """"""
 import os
 import sys
 import unittest
+from functools import lru_cache
 from hashlib import md5
 from itertools import count
 from pathlib import Path
@@ -28,24 +29,27 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
-def mdfive(s):
-    return md5(s.encode()).hexdigest()
+@lru_cache(maxsize=None)
+def mdfive(s, stretches):
+    for _ in range(stretches):
+        s = md5(s.encode()).hexdigest()
+    return s
 
 
-def md5i(salt, idx):
-    return mdfive(f"{salt}{idx}")
+def md5i(salt, idx, stretches):
+    return mdfive(f"{salt}{idx}", stretches)
 
 
-def index(salt):
+def index(salt, stretches):
     found = 0
     for i in count():
-        digest = md5i(salt, i)
+        digest = md5i(salt, i, stretches)
         for x in range(len(digest) - 2):
             if digest[x] == digest[x + 1] == digest[x + 2]:
                 c = digest[x]
                 is_key = False
                 for k in range(1, 1001):
-                    digest2 = md5i(salt, i + k)
+                    digest2 = md5i(salt, i + k, stretches)
                     for y in range(len(digest2) - 5):
                         if digest2[y:y + 5] == c * 5:
                             is_key = True
@@ -59,12 +63,12 @@ def index(salt):
                 break  # only check the first
 
 
-def part_1(source):
-    return index(source)
+def part_1(source, stretch=1):
+    return index(source, stretch)
 
 
-def part_2(source):
-    return None
+def part_2(source, stretch=2017):
+    return index(source, stretch)
 
 
 class UnitTests(unittest.TestCase):
@@ -80,13 +84,13 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(22728, part_1(self.test_source))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
+        self.assertEqual(23890, part_1(self.source))
 
     def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(22551, part_2(self.test_source))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(22696, part_2(self.source))
 
 
 if __name__ == '__main__':
