b11153f8eeee3aa5c7a55c5076f71b797f14a6aa
Ivo Woltring <ivo@ivonet.nl>
2022-01-08 14:28:13 +0100

    2016 day 19 part 1 dcll trial

--- 2016/day_19/day_19.py
+++ 2016/day_19/day_19.py
@@ -12,6 +12,7 @@ import sys
 import unittest
 from itertools import count
 from pathlib import Path
+from typing import Optional
 
 from ivonet.files import read_data
 from ivonet.iter import ints
@@ -27,30 +28,140 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
-def part_1a(source):
-    nr_of_elves = int(source)
-    elves = {k: True for k in range(int(nr_of_elves))}
-    for i in count():
-        elf = i % nr_of_elves
-        if elves[elf]:
-            for l in count(i + 1):
-                lelf = l % nr_of_elves
-                if lelf != i and elves[lelf]:
-                    elves[lelf] = False
-                    break
-        left = [k for k, v in elves.items() if v]
-        if len(left) == 1:
-            return left[0] + 1
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+    def __repr__(self) -> str:
+        return f"Node<data={repr(self.data)} prev={repr(self.prev.data)} next={repr(self.next.data)}>"
+
+
+class CircularDoublyLinkedList:
+    def __init__(self):
+        self.first: Node = Optional[Node]
+        self.current: Node = Optional[Node]
+
+    def __repr__(self) -> str:
+        """Representation of one cicle from the first node to the last node walking right"""
+        if not self.first:
+            return "CircularDoublyLinkedList<>"
+        ret = "CircularDoublyLinkedList<["
+        node = self.first
+        while True:
+            ret += f"{repr(node)}, "
+            node = node.next
+            if node == self.first:
+                break
+        return ret + "]>"
+
+    def get_node(self, index):
+        current = self.first
+        for _ in range(index):
+            current = current.next
+        self.current = current
+        return current
+
+    def next(self):
+        if not self.first:
+            return None
+        if not self.current:
+            self.current = self.first
+        self.current = self.current.next
+        return self.current
+
+    def previous(self):
+        if not self.first:
+            return None
+        if not self.current:
+            self.current = self.first
+        self.current = self.current.prev
+        return self.current
+
+    def reset(self):
+        self.current = self.first
+
+    @staticmethod
+    def insert_after(ref_node, new_node):
+        new_node.prev = ref_node
+        new_node.next = ref_node.next
+        new_node.next.prev = new_node
+        ref_node.next = new_node
+
+    def insert_before(self, ref_node, new_node):
+        self.insert_after(ref_node.prev, new_node)
+
+    def insert_at_end(self, new_node):
+        if self.first is None:
+            self.first = new_node
+            new_node.next = new_node
+            new_node.prev = new_node
+        else:
+            self.insert_after(self.first.prev, new_node)
+
+    def append(self, value):
+        self.insert_at_end(Node(value))
+
+    def insert_at_beginning(self, new_node):
+        self.insert_at_end(new_node)
+        self.first = new_node
+
+    def prepend(self, value):
+        self.insert_at_beginning(Node(value))
+
+    def remove(self, node):
+        if self.first.next == self.first:
+            self.first = None
+        else:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            if self.first == node:
+                self.first = node.next
+            if self.current == node:
+                self.current = self.first
+
+    def delete_at_index(self, index):
+        node = self.get_node(index)
+        if node:
+            self.remove(node)
+
+    def display(self):
+        if self.first is None:
+            return
+        current = self.first
+        while True:
+            print(current.data, end=' ')
+            current = current.next
+            if current == self.first:
+                break
+
+    def __len__(self) -> int:
+        node = self.first
+        for i in count():
+            node = node.next
+            if node == self.first:
+                return i
+
+    def get(self):
+        return self.current.data
+
+
+def create_cll(elves):
+    elf_ring = CircularDoublyLinkedList()
+    for i in range(1, elves + 1):
+        elf_ring.append(i)
+    return elf_ring
 
 
 def part_1(source):
     nr_of_elves = int(source)
-    elves = list(range(1, int(nr_of_elves) + 1))
-    elf = 0
-    while len(elves) > 1:
-        elf = (elf + 1) % len(elves)
-        elves.pop(elf)
-    return elves[0]
+    elf_ring = create_cll(nr_of_elves)
+    start_elf = elf_ring.get_node(0)
+    while len(elf_ring) > 1:
+        elf_ring.remove(start_elf.next)
+        start_elf = start_elf.next
+    return elf_ring.get()
 
 
 def part_2(source):
@@ -70,9 +181,6 @@ class UnitTests(unittest.TestCase):
 
     def test_part_1(self):
         self.assertEqual(None, part_1(self.source))
-
-    def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
 
     def test_part_2(self):
         self.assertEqual(None, part_2(self.source))
