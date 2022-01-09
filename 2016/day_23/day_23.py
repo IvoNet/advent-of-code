4b2b6e10e29a17b4cf723a38bd7bc3f727343fcb
Ivo Woltring <ivo@ivonet.nl>
2022-01-09 11:23:13 +0100

    2016 day 23 part 1

--- 2016/day_23/day_23.py
+++ 2016/day_23/day_23.py
@@ -26,8 +26,81 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
-def part_1(source):
+def prepare(source):
+    program = []
+    for line in source:
+        tmp = line.strip().split()
+        cmd = [tmp[0]]
+        for x in tmp[1:]:
+            try:
+                cmd.append(int(x))
+            except ValueError:
+                cmd.append(x)
+        program.append(cmd)
+    return program
+
+
+def assembunny(source, a=0, b=0, c=0, d=0):
+    # see 07 and 23 of 2015
+    program = prepare(source)
+    register = {
+        "a": a,
+        "b": b,
+        "c": c,
+        "d": d,
+    }
+    i = 0
+    while True:
+        _(register)
+        try:
+            cmd = program[i]
+            _(" ".join([str(x) for x in cmd]))
+        except IndexError:
+            return register
+        if cmd[0] == "tgl":
+            to_tgl = i + register[cmd[1]]
+            if to_tgl < 0 or to_tgl >= len(program):
+                i += 1
+                continue
+            tgl_cmd = program[to_tgl]
+            if len(tgl_cmd) == 2:
+                tgl_cmd[0] = "dec" if tgl_cmd[0] == "inc" else "inc"
+            else:  # len(tgl_cmd) == 3:
+                tgl_cmd[0] = "cpy" if tgl_cmd[0] == "jnz" else "jnz"
+            program[to_tgl] = tgl_cmd
+            i += 1
+            continue
+        if cmd[0] == "cpy" and type(cmd[1]) == int:
+            register[cmd[2]] = cmd[1]
+            i += 1
+            continue
+        if cmd[0] == "cpy" and type(cmd[1]) != int:
+            register[cmd[2]] = register[cmd[1]]
+            i += 1
+            continue
+        if cmd[0] == "inc":
+            register[cmd[1]] += 1
+            i += 1
+            continue
+        if cmd[0] == "dec":
+            register[cmd[1]] -= 1
+            i += 1
+            continue
+        if cmd[0] == "jnz" and type(cmd[1]) == int and cmd[1] != 0:
+            i += cmd[2] if type(cmd[2]) == int else register[cmd[2]]
+            _(f"jnz to cmd {i}")
+            continue
+        if cmd[0] == "jnz" and register[cmd[1]] != 0:
+            i += cmd[2]
+            _(f"jnz to cmd {i}")
+            continue
+        _("No command going to next command:", cmd)
+        i += 1
     return None
+
+
+def part_1(source, a=7):
+    return assembunny(source, a=a)["a"]
 
 
 def part_2(source):
@@ -41,10 +114,10 @@ class UnitTests(unittest.TestCase):
             print()
         day = str(ints(Path(__file__).name)[0])
         self.source = read_rows(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
+        self.test_source = read_rows(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}_test_a.input")
 
     def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+        self.assertEqual(3, part_1(self.test_source, a=0))
 
     def test_part_1(self):
         self.assertEqual(None, part_1(self.source))
