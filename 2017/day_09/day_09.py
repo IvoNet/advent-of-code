e615c1db4c76c3c1ed0c56c573452e24ca0239b2
Ivo Woltring <ivo@ivonet.nl>
2022-01-10 23:51:38 +0100

    2017 day 9 part 1

--- 2017/day_09/day_09.py
+++ 2017/day_09/day_09.py
@@ -11,8 +11,9 @@ import os
 import sys
 import unittest
 from pathlib import Path
+from queue import LifoQueue
 
-from ivonet.files import read_rows
+from ivonet.files import read_data
 from ivonet.iter import ints
 
 sys.dont_write_bytecode = True
@@ -26,8 +27,49 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
+class GarbageCollector:
+
+    def __init__(self, source) -> None:
+        self.garbage_stream = source
+        self.bucket = LifoQueue()
+        self.ignore_next = False
+        self.expected = None
+        self.score = 0
+        self.garbage_mode = False
+
+    def collect(self) -> GarbageCollector:
+        for ch in self.garbage_stream:
+            if self.ignore_next:
+                self.ignore_next = False
+                continue
+            if ch == "!":
+                self.ignore_next = True
+                continue
+            if self.garbage_mode:
+                if ch != ">":
+                    # We could collect garbage here
+                    continue
+                self.garbage_mode = False
+                continue
+            if ch == "<":
+                self.garbage_mode = True
+                continue
+            if ch == ",":
+                continue
+            if ch == "{":
+                self.bucket.put("}")
+                continue
+            group = self.bucket.get()
+            assert ch == group, "should be a group"
+            if self.bucket.empty():
+                self.score += 1
+            else:
+                self.score += self.bucket.qsize() + 1
+        return self
+
+
 def part_1(source):
-    return None
+    return GarbageCollector(source).collect().score
 
 
 def part_2(source):
@@ -40,11 +82,18 @@ class UnitTests(unittest.TestCase):
         if DEBUG:
             print()
         day = str(ints(Path(__file__).name)[0])
-        self.source = read_rows(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
+        self.source = read_data(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")
+        self.test_source = read_data("""""")
 
-    def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+    def test_example_data_1_part_1(self):
+        self.assertEqual(1, part_1("{}"))
+        self.assertEqual(6, part_1("{{{}}}"))
+        self.assertEqual(5, part_1("{{},{}}"))
+        self.assertEqual(16, part_1("{{{},{},{{}}}}"))
+        self.assertEqual(1, part_1("{<a>,<a>,<a>,<a>}"))
+        self.assertEqual(9, part_1("{{<ab>},{<ab>},{<ab>},{<ab>}}"))
+        self.assertEqual(9, part_1("{{<!!>},{<!!>},{<!!>},{<!!>}}"))
+        self.assertEqual(3, part_1("{{<a!>},{<a!>},{<a!>},{<ab>}}"))
 
     def test_part_1(self):
         self.assertEqual(None, part_1(self.source))
