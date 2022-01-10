5cfb046ffe1b165a31d0931f87f3ab01865c19e3
Ivo Woltring <ivo@ivonet.nl>
2022-01-10 15:49:57 +0100

    2017 day 6 done

--- 2017/day_06/day_06.py
+++ 2017/day_06/day_06.py
@@ -10,14 +10,15 @@ __doc__ = """"""
 import os
 import sys
 import unittest
+from itertools import count
 from pathlib import Path
 
-from ivonet.files import read_rows
+from ivonet.files import read_data
 from ivonet.iter import ints
 
 sys.dont_write_bytecode = True
 
-DEBUG = True
+DEBUG = False
 
 
 # noinspection DuplicatedCode
@@ -26,12 +27,27 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
-def part_1(source):
-    return None
+def part_1(source, part2=False):
+    reg = ints(source)
+    seen = []
+    for steps in count(1):
+        dist = max(reg)
+        pos = reg.index(dist)
+        reg[pos] = 0
+        for idx in range(pos + 1, pos + 1 + dist):
+            reg[idx % len(reg)] += 1
+
+        state = tuple(x for x in reg)
+        _(state)
+        if state in seen:
+            if part2:
+                return steps - 1 - seen.index(state)
+            return steps
+        seen.append(state)
 
 
 def part_2(source):
-    return None
+    return part_1(source, part2=True)
 
 
 class UnitTests(unittest.TestCase):
@@ -40,20 +56,20 @@ class UnitTests(unittest.TestCase):
         if DEBUG:
             print()
         day = str(ints(Path(__file__).name)[0])
-        self.source = read_rows(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
+        self.source = read_data(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")
+        self.test_source = read_data("""0 2 7 0""")
 
     def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+        self.assertEqual(5, part_1(self.test_source))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
+        self.assertEqual(4074, part_1(self.source))
 
     def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(4, part_2(self.test_source))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(2793, part_2(self.source))
 
 
 if __name__ == '__main__':
