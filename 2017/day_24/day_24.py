4916a2d79a41dcf1a5f18ec623b32d82071b349c
Ivo Woltring <ivo@ivonet.nl>
2022-01-15 23:23:36 +0100

    2017 day 24 Done (not happy!)

--- 2017/day_24/day_24.py
+++ 2017/day_24/day_24.py
@@ -10,14 +10,11 @@ __doc__ = """"""
 import os
 import sys
 import unittest
-from collections import defaultdict
 from pathlib import Path
-from typing import TypeVar, Callable
+from typing import TypeVar
 
-from ivonet.collection import Queue, PriorityQueue
 from ivonet.files import read_rows
 from ivonet.iter import ints
-from ivonet.search import Node, node_to_path
 
 sys.dont_write_bytecode = True
 T = TypeVar('T')
@@ -32,93 +29,47 @@ def _(*args, end="\n"):
 
 
 def parse(source):
-    ret = defaultdict(set)
-    for line in source:
-        a, b = ints(line)
-        ret[a].add(b)
-        ret[b].add(a)
-    return ret
+    return [(a, b) if a < b else (b, a) for a, b in [ints(line) for line in source]]
 
 
-def bfs(start: T, successors: Callable[[T], list[T]]):
-    """Breath first search
-    """
-    # frontier is where we've yet to go
-    strongest = float("-inf")
-
-    for initial in successors(start):
-        frontier: Queue[Node[T]] = Queue()
-        frontier.push(Node(initial, None))
-        # explored is where we've been
-        explored: set[T] = {initial}
-        # keep going while there is more to explore
-        while not frontier.empty:
-            current_node: Node[T] = frontier.pop()
-            current_state: T = current_node.state
-            path = node_to_path(current_node)
-            strength = sum(x + y for x, y in path)
-            # all_paths.append((strength, path))
-            strongest = max(strongest, strength)
-            # check where we can go next and haven't explored
-            for child in successors(current_state[1]):
-                if child in explored:  # skip children we already explored
-                    continue
-                explored.add(child)
-                frontier.push(Node(child, current_node))
-    return strongest  # , strongest_path, all_paths, explored
+def extend(bridge, blocks):
+    unused = list(filter(lambda b: b not in bridge and b[::-1] not in bridge, blocks))
+    for block in unused:
+        if bridge[-1][1] == block[0]:
+            yield bridge + [block]
+        elif bridge[-1][1] == block[1]:
+            yield bridge + [block[::-1]]
 
 
-def astar(start: T,
-          successors: Callable[[T], list[T]],
-          cost: Callable[[T], int] = lambda z: z[0] + z[1]):
-    strongest = float("-inf")
-    strongest_path = None
-    all_paths = []
-
-    for initial in successors(start):
-        frontier: PriorityQueue[Node[T]] = PriorityQueue()
-        frontier.push(Node(initial, None, 0.0, 0.0))
-        explored: dict[T, float] = {initial: 0.0}
-
-        # keep going while there is more to explore
-        while not frontier.empty:
-            current_node: Node[T] = frontier.pop()
-            current_state: T = current_node.state
-            # if we found the goal, we're done
-            path = node_to_path(current_node)
-            strength = sum(x + y for x, y in path)
-            all_paths.append((strength, path))
-            if strength > strongest:
-                strongest_path = path
-                strongest = strength
-            for nb in successors(current_state):
-                new_cost: float = current_node.cost + cost(nb)
-
-                if nb not in explored or explored[nb] > new_cost:
-                    explored[nb] = new_cost
-                    frontier.push(Node(nb, current_node, new_cost, 0.0))
-    return strongest
+def bridge_strength(bridge):
+    return sum(map(sum, bridge))
 
 
-def can_connect(magnets) -> Callable[[list[tuple[int, int]]], list[T]]:
-    def connectors(a: T) -> list[T]:
-        ret = set()
-        for b in magnets[a]:
-            ret.add((a, b) if a <= b else (b, a))
-        return list(ret)
-
-    return connectors
+def astar(magnets):
+    new_bridges = [[magnet] for magnet in magnets if 0 in magnet]  # Need to start with 0
+    strongest = 0
+    longest_strength = 0
+    while new_bridges:
+        bridges = new_bridges
+        new_bridges = []
+        for bridge in bridges:
+            new_bridges.extend(list(extend(bridge, magnets)))
+        if new_bridges:
+            longest_strength = max(bridge_strength(bridge) for bridge in new_bridges)
+            strongest = max(strongest, longest_strength)
+    return strongest, longest_strength
 
 
 def part_1(source):
     magnets = parse(source)
-    connectors = can_connect(magnets)
-    strongest = bfs(0, connectors)
+    strongest, _ = astar(magnets)
     return strongest
 
 
 def part_2(source):
-    return None
+    magnets = parse(source)
+    _, longest_strength = astar(magnets)
+    return longest_strength
 
 
 class UnitTests(unittest.TestCase):
@@ -141,13 +92,10 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(31, part_1(self.test_source))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
-
-    def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(1868, part_1(self.source))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(1841, part_2(self.source))
 
 
 if __name__ == '__main__':
