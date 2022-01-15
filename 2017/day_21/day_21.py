553c1afbcb37ea94c9f15e4559c0d38955393de1
Ivo Woltring <ivo@ivonet.nl>
2022-01-15 13:36:45 +0100

    2017 day 21 working

--- 2017/day_21/day_21.py
+++ 2017/day_21/day_21.py
@@ -19,7 +19,13 @@ from ivonet.str import rotate_right
 
 sys.dont_write_bytecode = True
 
-DEBUG = True
+BASE_GRID = [
+    ".#.",
+    "..#",
+    "###",
+]
+
+DEBUG = False
 
 
 # noinspection DuplicatedCode
@@ -49,7 +55,8 @@ def diagonal(key):
         nk.append("".join(l))
     return tuple(nk)
 
-def parse(source):
+
+def parse_old(source):
     """Parse the source and
     - add all the flipped and rotated combinations of the same key to the rules
       with its value
@@ -67,16 +74,31 @@ def parse(source):
                 k = rotate(c, i)
                 rules[k] = value
                 rules[diagonal(k)] = value
-
     _(rules)
     return rules
 
 
-BASE_GRID = [
-    ".#.",
-    "..#",
-    "###",
-]
+def parse(source):
+    rules = {}
+    for line in source:
+        k, v = line.strip().split(" => ")
+        k = tuple(k.split("/"))
+        v = v.split("/")
+        rules[k] = v
+        rules[diagonal(k)] = v
+
+        k2 = tuple([s[::-1] for s in k])
+        rules[k2] = v
+        rules[diagonal(k2)] = v
+
+        k3 = tuple(s for s in k[::-1])
+        rules[k3] = v
+        rules[diagonal(k3)] = v
+
+        k4 = tuple([s[::-1] for s in k3])
+        rules[k4] = v
+        rules[diagonal(k4)] = v
+    return rules
 
 
 class ArtProgram:
@@ -89,7 +111,6 @@ class ArtProgram:
     def step(self, times=1):
         for _ in range(times):
             self.enhance()
-            _(self.grid)
 
     def enhance(self):
         if len(self.grid) % 2 == 0:
@@ -102,8 +123,8 @@ class ArtProgram:
     def enhance_odd(self):
         _("Enhance odd")
         new_grid = []
-        new_lines = [[], [], [], []]
         for h in range(0, len(self.grid), 3):
+            new_lines = [[], [], [], []]
             for w in range(0, len(self.grid), 3):
                 k = tuple([self.grid[h][w:w + 3], self.grid[h + 1][w:w + 3], self.grid[h + 2][w:w + 3]])
                 v = self.rules[k]
@@ -115,30 +136,33 @@ class ArtProgram:
     def enhance_even(self):
         _("Enhance even")
         new_grid = []
-        new_lines = [[], [], []]
         for h in range(0, len(self.grid), 2):
+            new_lines = [[], [], []]
             for w in range(0, len(self.grid), 2):
                 k = tuple([self.grid[h][w:w + 2], self.grid[h + 1][w:w + 2]])
                 v = self.rules[k]
                 for i, l in enumerate(v):
                     new_lines[i].extend(list(l))
-        new_grid.extend(["".join(l) for l in new_lines])
+            new_grid.extend(["".join(l) for l in new_lines])
         self.grid = new_grid
 
     def lit_pixels(self):
         return sum([sum([c == "#" for c in l]) for l in self.grid])
 
+    def __repr__(self) -> str:
+        return repr(self.grid)
+
 
 def part_1(source, times=5):
     rules = parse(source)
     art_program = ArtProgram(rules)
-    for i in range(times):
-        art_program.step()
+    art_program.step(times)
+    _(art_program)
     return art_program.lit_pixels()
 
 
-def part_2(source):
-    return None
+def part_2(source, times=18):
+    return part_1(source, times=times)
 
 
 class UnitTests(unittest.TestCase):
@@ -154,13 +178,10 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(12, part_1(self.test_source, times=2))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
-
-    def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(171, part_1(self.source))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(2498142, part_2(self.source))
 
 
 if __name__ == '__main__':
