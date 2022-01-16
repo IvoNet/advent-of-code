2967e15e4a47faaeed1bc0c01ceb874129d2d374
Ivo Woltring <ivo@ivonet.nl>
2022-01-16 19:16:27 +0100

    2018 day 05 done

--- 2018/day_05/day_05.py
+++ 2018/day_05/day_05.py
@@ -8,16 +8,17 @@ __license__ = "Apache 2.0"
 __doc__ = """"""
 
 import os
+import re
 import sys
 import unittest
 from pathlib import Path
 
-from ivonet.files import read_rows
+from ivonet.files import read_data
 from ivonet.iter import ints
 
 sys.dont_write_bytecode = True
 
-DEBUG = True
+DEBUG = False
 
 
 # noinspection DuplicatedCode
@@ -26,12 +27,40 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
+def reacting(source, remove=None):
+    l = "abcdefghijklmnopqrstuvwxyz"
+    lu = "|".join(["".join(x) for x in list(zip(l, l.upper()))])
+    ul = "|".join(["".join(x) for x in list(zip(l.upper(), l))])
+    regex = f"{lu}|{ul}"
+    pat = re.compile(regex)
+    ret = source
+    if remove:
+        ret = re.sub(remove, "", ret)
+    while True:
+        exploded = pat.sub("", ret)
+        if len(exploded) == len(ret):
+            break
+        ret = exploded
+    return len(ret), ret
+
+
 def part_1(source):
-    return None
+    return reacting(source)[0]
 
 
 def part_2(source):
-    return None
+    l = "abcdefghijklmnopqrstuvwxyz"
+    units = ["|".join(x) for x in list(zip(l, l.upper()))]
+    shortest_polymer = None
+    shortest = float("inf")
+    for unit in units:
+        ret = source
+        ret = re.sub(unit, "", ret)
+        length, polymer = reacting(ret)
+        if length < shortest:
+            shortest_polymer = polymer
+            shortest = length
+    return len(shortest_polymer)
 
 
 class UnitTests(unittest.TestCase):
@@ -40,20 +69,20 @@ class UnitTests(unittest.TestCase):
         if DEBUG:
             print()
         day = str(ints(Path(__file__).name)[0])
-        self.source = read_rows(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
+        self.source = read_data(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")
+        self.test_source = read_data("""dabAcCaCBAcCcaDA""")
 
     def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+        self.assertEqual(10, part_1(self.test_source))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
+        self.assertEqual(10972, part_1(self.source))
 
     def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(4, part_2(self.test_source))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(5278, part_2(self.source))
 
 
 if __name__ == '__main__':
