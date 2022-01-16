952565dfa1c093f584cd665a41c028d40cae4973
Ivo Woltring <ivo@ivonet.nl>
2022-01-16 16:47:32 +0100

    2018 day 04 part 1

--- 2018/day_04/day_04.py
+++ 2018/day_04/day_04.py
@@ -10,6 +10,7 @@ __doc__ = """"""
 import os
 import sys
 import unittest
+from collections import defaultdict
 from pathlib import Path
 
 from ivonet.files import read_rows
@@ -17,7 +18,7 @@ from ivonet.iter import ints
 
 sys.dont_write_bytecode = True
 
-DEBUG = True
+DEBUG = False
 
 
 # noinspection DuplicatedCode
@@ -26,8 +27,49 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
+def process(source):
+    chonological = sorted(source, key=lambda x: (x[6:8], x[9:11], x[12:14], x[15:17]))
+    sleep = defaultdict(lambda: defaultdict(list))  # sleep[guard][day] = [minute,...]
+    guard = None
+    falls = None
+    for line in chonological:
+        _(line)
+        numbers = ints(line)
+        day = "-".join(str(x) for x in numbers[:3])
+        if "Guard" in line:
+            guard = numbers[-1]
+            falls = None
+        elif "falls" in line:
+            falls = numbers[4]
+        elif "wakes" in line:
+            wakes = numbers[4]
+            assert falls is not None, line
+            sleep[guard][day].extend(range(falls, wakes))
+            falls = None
+    return sleep
+
+
+def sleepiest_guard(sleep):
+    """The guard with the most minutes asleep"""
+    return max(sleep, key=lambda guard: sum(map(len, sleep[guard].values())))
+
+
+def sleepiest_minute(guard, sleep):
+    """Which minute is he asleep the most often"""
+
+    def times_asleep(minute):
+        """Counts the number of times this guard is asleep on a specific minute"""
+        return sum(minute in minutes for minutes in sleep[guard].values())
+
+    return max(range(60), key=times_asleep)
+
+
 def part_1(source):
-    return None
+    sleep = process(source)
+    guard = sleepiest_guard(sleep)
+    minute = sleepiest_minute(guard, sleep)
+    _(guard, minute)
+    return guard * minute
 
 
 def part_2(source):
@@ -41,13 +83,29 @@ class UnitTests(unittest.TestCase):
             print()
         day = str(ints(Path(__file__).name)[0])
         self.source = read_rows(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
+        self.test_source = read_rows("""[1518-11-01 00:00] Guard #10 begins shift
+[1518-11-01 00:05] falls asleep
+[1518-11-01 00:25] wakes up
+[1518-11-01 00:30] falls asleep
+[1518-11-01 00:55] wakes up
+[1518-11-01 23:58] Guard #99 begins shift
+[1518-11-02 00:40] falls asleep
+[1518-11-02 00:50] wakes up
+[1518-11-03 00:05] Guard #10 begins shift
+[1518-11-03 00:24] falls asleep
+[1518-11-03 00:29] wakes up
+[1518-11-04 00:02] Guard #99 begins shift
+[1518-11-04 00:36] falls asleep
+[1518-11-04 00:46] wakes up
+[1518-11-05 00:03] Guard #99 begins shift
+[1518-11-05 00:45] falls asleep
+[1518-11-05 00:55] wakes up""")
 
     def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+        self.assertEqual(240, part_1(self.test_source))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
+        self.assertEqual(3212, part_1(self.source))
 
     def test_example_data_part_2(self):
         self.assertEqual(None, part_2(self.test_source))
--- unittest.sh
+++ unittest.sh
@@ -3,4 +3,5 @@ if [ -z "$VIRTUAL_ENV" ]; then
     echo "Please start the virtual env first..."
     exit 1
 fi
+clear
 python -m unittest discover -p tests.py -v
