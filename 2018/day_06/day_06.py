fbbe3bdf53748bd03e95b59a26763413553a6742
Ivo Woltring <ivo@ivonet.nl>
2022-01-16 19:47:43 +0100

    2018 day 06 wip

--- 2018/day_06/day_06.py
+++ 2018/day_06/day_06.py
@@ -11,6 +11,7 @@ import os
 import sys
 import unittest
 from pathlib import Path
+from typing import NamedTuple
 
 from ivonet.files import read_rows
 from ivonet.iter import ints
@@ -26,7 +27,22 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
+class Location(NamedTuple):
+    col: int
+    row: int
+
+
+def parse(source):
+    return sorted([Location(*ints(line)) for line in source], key=lambda x: (x.row, x.col), reverse=False)
+
+
+def manhatten_distance(left: Location, right: Location) -> int:
+    return abs(left.row - right.row) + abs(left.col - right.col)
+
+
 def part_1(source):
+    coords = parse(source)
+    _(coords)
     return None
 
 
@@ -41,10 +57,15 @@ class UnitTests(unittest.TestCase):
             print()
         day = str(ints(Path(__file__).name)[0])
         self.source = read_rows(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
+        self.test_source = read_rows("""1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9""")
 
     def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+        self.assertEqual(17, part_1(self.test_source))
 
     def test_part_1(self):
         self.assertEqual(None, part_1(self.source))
