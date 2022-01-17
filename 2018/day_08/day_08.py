9079f3c781520aa8570548075aa84bb55a5c0abb
Ivo Woltring <ivo@ivonet.nl>
2022-01-17 22:51:42 +0100

    2018 day 08 Done

--- 2018/day_08/day_08.py
+++ 2018/day_08/day_08.py
@@ -29,26 +29,34 @@ def _(*args, end="\n"):
 
 class Tree(NamedTuple):
     kids: int
-    metadata: list
+    metadata: int
 
 
-def parse(source):
-    kids, num_metas, rest = source[0], source[1], source[2:]
+def license_parser(source):
+    kids, metadata_entries, rest = source[0], source[1], source[2:]
     total = 0
+    metadatas = []
     for _ in range(kids):
-        sub_total, rest = parse(rest)
+        sub_total, metadata_sum, rest = license_parser(rest)
         total += sub_total
-    metas, rest = rest[:num_metas], rest[num_metas:]
+        metadatas.append(metadata_sum)
+    metas, rest = rest[:metadata_entries], rest[metadata_entries:]
     total += sum(metas)
-    return total, rest
+    meta_sum = total
+    if kids:
+        meta_sum = 0
+        for i in metas:
+            if i <= len(metadatas):
+                meta_sum += metadatas[i - 1]
+    return total, meta_sum, rest
 
 
 def part_1(source):
-    return parse(source)[0]
+    return license_parser(source)[0]
 
 
 def part_2(source):
-    return None
+    return license_parser(source)[1]
 
 
 class UnitTests(unittest.TestCase):
@@ -67,10 +75,10 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(41028, part_1(self.source))
 
     def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(66, part_2(self.test_source))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(20849, part_2(self.source))
 
 
 if __name__ == '__main__':
