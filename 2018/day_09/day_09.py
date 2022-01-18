8a439bc65b7c41fbd481bf74a016527ca975201e
Ivo Woltring <ivo@ivonet.nl>
2022-01-18 08:57:53 +0100

    2018 day 09 Done - Cleanup

--- 2018/day_09/day_09.py
+++ 2018/day_09/day_09.py
@@ -10,14 +10,14 @@ __doc__ = """"""
 import os
 import sys
 import unittest
-from collections import defaultdict
+from collections import defaultdict, deque
 from dataclasses import dataclass
 from itertools import count
 from pathlib import Path
 
 from ivonet.cdll import CircularDoublyLinkedList
 from ivonet.files import read_data
-from ivonet.iter import ints
+from ivonet.iter import ints, range_inc
 
 sys.dont_write_bytecode = True
 
@@ -36,33 +36,29 @@ def marble_game(players=403, last_marble=71920):
     board = CircularDoublyLinkedList()
     board.append(0)
     score = defaultdict(int)
-    for i in count(1):
-        player = (i - 1) % players + 1
-        _("Player:", player)
-        _(board.repr_data())
-        if i % 23 == 0:
-            score[player] += i
-            seven_left = board.step_left(7)
+    for m in count(1):
+        player = (m - 1) % players + 1
+        if m % 23 == 0:
+            score[player] += m
+            board.step_left(7)
             score[player] += board.get()
             board.remove(board.current())
-            _("Score:", score[player], "Seven left:", seven_left.data, "Current node", board.current())
         else:
             board.step_right(1)
-            board.insert_after_current(i)
+            board.insert_after_current(m)
             board.next()
-            _("Current node:", board.current())
-        if i == last_marble:
+        if m == last_marble:
             break
-    _(score)
-    _(board.repr_data())
+        if DEBUG:
+            print(board.repr_data())
     return score
 
 
 @dataclass
 class Marble:
     value: int
-    before: Marble = None
-    after: Marble = None
+    before: Optional[Marble] = None
+    after: Optional[Marble] = None
 
 
 def marble_game_v2(players=403, marbles=71920):
@@ -71,9 +67,10 @@ def marble_game_v2(players=403, marbles=71920):
     current.before = current
     current.after = current
     player = 0
-    for placed in range(1, marbles + 1):
-        if placed % 23 == 0:
-            scores[player] += placed
+    for m in range(1, marbles + 1):
+        player = (m - 1) % players + 1
+        if m % 23 == 0:
+            scores[player] += m
             for _ in range(7):
                 current = current.before
             before = current.before
@@ -85,11 +82,31 @@ def marble_game_v2(players=403, marbles=71920):
         else:
             before = current.after
             after = before.after
-            new = Marble(placed, before, after)
+            new = Marble(m, before, after)
             before.after = new
             after.before = new
             current = new
-        player = player % players + 1
+    return scores
+
+
+def marble_game_v3(players=403, marbles=71920):
+    """Best performing for now
+    deque is effective as we can rotate.
+    If you look at the example the current is effectively always the last -1
+    """
+    board = deque([0])
+    scores = defaultdict(int)
+    for m in range_inc(1, marbles):
+        player = (m - 1) % players + 1
+        if m % 23 == 0:
+            board.rotate(7)
+            scores[player] += board.pop() + m
+            board.rotate(-1)
+        else:
+            board.rotate(-1)
+            board.append(m)
+        if DEBUG:
+            print(player, list(board))
     return scores
 
 
@@ -102,8 +119,7 @@ def part_1(source):
 
 def part_2(source):
     players, marble = ints(source)
-    score = marble_game_v2(players, marble * 100)
-    _(score)
+    score = marble_game_v3(players, marble * 100)
     return max(score.values())
 
 
@@ -118,13 +134,17 @@ class UnitTests(unittest.TestCase):
     def test_example_1_part_1(self):
         self.assertEqual(32, max(marble_game(9, 25).values()))
 
-    def test_example_2_part_1(self):
+    def test_example_v1_2_part_1(self):
         self.assertEqual(8317, max(marble_game(10, 1618).values()))
 
-    def test_example_3_part_1(self):
+    def test_example_v1_3_part_1(self):
         self.assertEqual(146373, max(marble_game(13, 7999).values()))
 
-    def test_example_v2_1_part_1(self):
+    @unittest.SkipTest  # Don't try this one.... takes forever but does work :-)
+    def test_example_v1_3_part_1(self):
+        self.assertEqual(3668541094, max(marble_game(403, 7192000).values()))
+
+    def test_example_v1_v2_1_part_1(self):
         self.assertEqual(32, max(marble_game_v2(9, 25).values()))
 
     def test_example_v2_2_part_1(self):
@@ -132,6 +152,23 @@ class UnitTests(unittest.TestCase):
 
     def test_example_v2_3_part_1(self):
         self.assertEqual(146373, max(marble_game_v2(13, 7999).values()))
+
+    @unittest.SkipTest  # works but a takes about 11 seconds... v3 is better
+    def test_example_v2_4_part_2(self):
+        self.assertEqual(3668541094, max(marble_game_v2(403, 7192000).values()))
+
+    def test_example_v3_1_part_1(self):
+        self.assertEqual(32, max(marble_game_v3(9, 25).values()))
+
+    def test_example_v3_2_part_1(self):
+        self.assertEqual(8317, max(marble_game_v3(10, 1618).values()))
+
+    def test_example_v3_3_part_1(self):
+        self.assertEqual(146373, max(marble_game_v3(13, 7999).values()))
+
+    @unittest.SkipTest  # Already done in the official part_2 test
+    def test_example_v3_4_part_2(self):
+        self.assertEqual(3668541094, max(marble_game_v3(403, 7192000).values()))
 
     def test_part_1(self):
         self.assertEqual(439089, part_1(self.source))
