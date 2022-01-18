b2bd28eedb125ebb9ee45fe902fd84e972157577
Ivo Woltring <ivo@ivonet.nl>
2022-01-18 22:41:27 +0100

    2018 day 11 part 1

--- 2018/day_11/day_11.py
+++ 2018/day_11/day_11.py
@@ -11,10 +11,11 @@ import os
 import sys
 import unittest
 from pathlib import Path
-from typing import NamedTuple
+from typing import NamedTuple, Iterable, Optional
 
-from ivonet.files import read_ints, read_data
-from ivonet.iter import ints
+from ivonet import infinite
+from ivonet.files import read_data
+from ivonet.iter import ints, rangei
 
 sys.dont_write_bytecode = True
 
@@ -28,8 +29,8 @@ def _(*args, end="\n"):
 
 
 class Coord(NamedTuple):
-    c: int
     r: int
+    c: int
 
 
 def hundreds_digit(nr: int) -> int:
@@ -43,12 +44,70 @@ def power_level(loc: Coord, grid_serial_nr=8868, plus=10):
     return hundreds_digit(((rack_id * loc.r) + grid_serial_nr) * rack_id) - 5
 
 
+def grid_iter(width=300, height=300, size=3):
+    for r in rangei(1, height - size):
+        for c in rangei(1, width - size):
+            yield range(r, r + size), range(c, c + size)
+
+
+def total_power(rows: Iterable, cols: Iterable, grid_serial_nr) -> int:
+    return sum(power_level(Coord(r, c), grid_serial_nr) for r in rows for c in cols)
+
+
+class PowerGrid:
+
+    def __init__(self, serial_number=8868) -> None:
+        self.width = 300
+        self.height = 300
+        self.serial = serial_number
+        self.grid = [[power_level(Coord(r=r, c=c), self.serial) for c in rangei(1, self.width)]
+                     for r in rangei(1, self.height)]
+        self.highest = -infinite
+        self.top_x = 0
+        self.top_y = 0
+        self.size = 0
+
+    def total_power(self, rows: Iterable, cols: Iterable) -> int:
+        return sum(self.grid[r][c] for r in rows for c in cols)
+
+    def highest_power(self):
+        highest: int = -infinite
+        top_left: Optional[Coord] = None
+        for rows, cols in grid_iter():
+            power = self.total_power(rows, cols)
+            if power > highest:
+                _(power)
+                top_left = Coord(r=rows[0], c=cols[0])
+                highest = power
+        return top_left.c, top_left.r
+
+
 def part_1(source, width=300, height=300):
-    return None
+    grid_serial_number: int = int(source)
+    highest: int = -infinite
+    top_left: Optional[Coord] = None
+    for rows, cols in grid_iter(width, height):
+        power = total_power(rows, cols, grid_serial_nr=grid_serial_number)
+        if power > highest:
+            _(power)
+            top_left = Coord(r=rows[0], c=cols[0])
+            highest = power
+    return top_left.c, top_left.r
 
 
 def part_2(source):
-    return None
+    grid_serial_number: int = int(source)
+    highest: int = -infinite
+    top_left: Optional[Coord] = None
+    top_grid_size: int = -infinite
+    for size in rangei(1, 300):
+        for rows, cols in grid_iter(size=size):
+            power = total_power(rows, cols, grid_serial_nr=grid_serial_number)
+            if power > highest:
+                top_left = Coord(r=rows[0], c=cols[0])
+                highest = power
+                top_grid_size = size
+    return top_left.c, top_left.r, top_grid_size
 
 
 class UnitTests(unittest.TestCase):
@@ -57,8 +116,7 @@ class UnitTests(unittest.TestCase):
         if DEBUG:
             print()
         day = str(ints(Path(__file__).name)[0])
-        self.source = read_ints(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")[0]
-        self.test_source = read_data("""""")
+        self.source = read_data(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")
 
     def test_hundreds(self):
         self.assertEqual(9, hundreds_digit(949))
@@ -67,18 +125,24 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(1, hundreds_digit(100))
 
     def test_power_level(self):
-        self.assertEqual(-5, power_level(Coord(122, 79), grid_serial_nr=57))
-        self.assertEqual(0, power_level(Coord(217, 196), grid_serial_nr=39))
-        self.assertEqual(4, power_level(Coord(101, 153), grid_serial_nr=71))
+        self.assertEqual(-5, power_level(Coord(c=122, r=79), grid_serial_nr=57))
+        self.assertEqual(0, power_level(Coord(c=217, r=196), grid_serial_nr=39))
+        self.assertEqual(4, power_level(Coord(c=101, r=153), grid_serial_nr=71))
 
     def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+        self.assertEqual((33, 45), part_1(18))
+
+    def test_example_data_2_part_1(self):
+        self.assertEqual((21, 61), part_1(42))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
+        self.assertEqual((241, 40), part_1(self.source))
 
     def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual((90, 269, 16), part_2(18))
+
+    def test_example_data_2_part_2(self):
+        self.assertEqual((21, 61), part_1(42))
 
     def test_part_2(self):
         self.assertEqual(None, part_2(self.source))
