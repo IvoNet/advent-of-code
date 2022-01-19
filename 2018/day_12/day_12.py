0f4895e7d9606e16ca50355527c51cd30f37936d
Ivo Woltring <ivo@ivonet.nl>
2022-01-19 16:01:17 +0100

    2018 day 12 done

--- 2018/day_12/day_12.py
+++ 2018/day_12/day_12.py
@@ -10,14 +10,15 @@ __doc__ = """"""
 import os
 import sys
 import unittest
+from itertools import islice
 from pathlib import Path
 
 from ivonet.files import read_rows
-from ivonet.iter import ints
+from ivonet.iter import ints, rangei
 
 sys.dont_write_bytecode = True
 
-DEBUG = True
+DEBUG = False
 
 
 # noinspection DuplicatedCode
@@ -27,18 +28,60 @@ def _(*args, end="\n"):
 
 
 def parse(source):
-    state = source[0].replace("initial state: ", "").strip()
-    notes = [line.split(" => ") for line in source[2:]]
-    return state, notes
+    plants = {i for i, c in enumerate(source[0].split()[-1]) if c == "#"}
+    notes = {k: v for k, v in [line.strip().split(" => ") for line in source[2:]]}
+    _(plants, notes)
+    return plants, notes
 
 
-def part_1(source, generations=325):
-    initial, notes = parse(source)
-    return None
+def lives(plants, i, notes):
+    """Determine if a plant lives, dies or spaws.
+    - As the c in llcrr is the one in focus for the notes we need to add two to the beginning and end of the list
+      and let them return "." when called for.
+    - then see if we need to grow from the notes
+    """
+    llcrr = "".join(".#"[j in plants] for j in rangei(i - 2, i + 2))
+    return notes.get(llcrr, ".") == "#"
 
 
-def part_2(source):
-    return None
+def next_gen(plants, notes):
+    """Generates the new generation based on the notes"""
+    while True:
+        yield plants
+        expand = rangei(min(plants) - 3, max(plants) + 3)
+        plants = {i for i in expand if lives(plants, i, notes)}
+
+
+def part_1(source, generations=20):
+    plants, notes = parse(source)
+    _(plants, notes)
+    grow = next_gen(plants, notes)
+    for i in rangei(0, generations):
+        plants = next(grow)
+        _(i, ":", "".join(".#"[j in plants] for j in range(min(plants), max(plants))))
+    return sum(plants)
+
+
+def nth_generation(grow, nth, constant_genertion=100):
+    """The sum of plants in the nth generation.
+    The assuming is that linear growth after constant_genertion generations mark
+    This you have to find out by increasing the part 1 generations to say 200 and print
+    the results. You will se the pattern then."""
+    plant_count_below_stable, first_stable_count = map(sum, islice(grow, constant_genertion, constant_genertion + 2))
+    return plant_count_below_stable + (nth - constant_genertion) * (first_stable_count - plant_count_below_stable)
+
+
+def part_2(source, generations=100):
+    """Ok 50 bilion gens is a bit much to brute forse
+    Observations:
+    - by increasing the number of generations (playing a bit) you notice that at about 100 gens
+      the groth becomes steady.
+    - in my case an increase of 80 every gen after that so I guess:
+    - (50_000_000_000 - 100) * 80 + gen(100)
+    - Now lets put that into a function to generify
+    """
+    plants, notes = parse(source)
+    return nth_generation(next_gen(plants, notes), 50 * 10 ** 9)
 
 
 class UnitTests(unittest.TestCase):
@@ -66,16 +109,13 @@ class UnitTests(unittest.TestCase):
 ####. => #""")
 
     def test_example_data_part_1(self):
-        self.assertEqual(".#....##....#####...#######....#.#..##.", part_1(self.test_source, generations=20))
+        self.assertEqual(325, part_1(self.test_source, generations=20))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
-
-    def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(3248, part_1(self.source))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(4000000000000, part_2(self.source))
 
 
 if __name__ == '__main__':
