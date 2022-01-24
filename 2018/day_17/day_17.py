06783f9150123b8fd092ef1d59aef474cec8c200
Ivo Woltring <ivo@ivonet.nl>
2022-01-24 23:14:51 +0100

    2018 day 17 wip

--- 2018/day_17/day_17.py
+++ 2018/day_17/day_17.py
@@ -32,8 +32,8 @@ class Cell(str, Enum):
     SAND = "."
     CLAY = "#"
     SETTLED_WATER = "~"
-    WATER = "|"
-    FAUCET = "+"
+    FLOWING_WATER = "|"
+    SPRING_OF_WATER = "+"
     GOAL = "G"
 
     def __repr__(self) -> str:
@@ -50,30 +50,110 @@ class Location(NamedTuple):
 
 class ReservoirResearch:
 
-    def __init__(self, source) -> None:
+    def __init__(self, source, init_spring: Location = Location(0, 500)) -> None:
         self.source = source
         self.rows: int = 0
+        self.min_rows: int = 0
         self.cols: int = 0
         self.grid = []
+        self.clay: list[int, int] = []
         self.init()
-        self.faucet = Location(0, 500)
-        self.grid[self.faucet.row][self.faucet.col] = Cell.FAUCET
+        self.springs: list[Location] = [init_spring]
+        self.settled_water: set[Location] = set()
+        self.flowing_water: set[Location] = set()
+        self.settled_springs: set[Location] = set()
 
     def init(self):
         start: Callable[[Any], list[list[int]]] = lambda i: [ints(line) for line in self.source if line.startswith(i)]
         y_start = start("y")
         x_start = start("x")
         self.rows = max(max(y_start, key=lambda x: x[0])[0], max(x_start, key=lambda x: x[2])[0])
+        self.min_rows = min(min(y_start, key=lambda x: x[0])[0], min(x_start, key=lambda x: x[1])[1])
         self.cols = max(max(y_start, key=lambda x: x[2])[0], max(x_start, key=lambda x: x[0])[0])
         self.grid = [[Cell.SAND for _ in rangei(0, self.cols)] for _ in rangei(0, self.rows)]
         yx = [[y, range(b, c)] for y, b, c in y_start]
         for r, cr in yx:
             for c in cr:
                 self.grid[r][c] = Cell.CLAY
+                self.clay.append((r, c))
         xy = [[x, range(b, c)] for x, b, c in x_start]
         for c, yr in xy:
             for r in yr:
                 self.grid[r][c] = Cell.CLAY
+                self.clay.append((r, c))
+
+    def mark(self):
+        for f in self.springs:
+            self.grid[f.row][f.col] = Cell.SPRING_OF_WATER
+        for f in self.settled_water:
+            self.grid[f.row][f.col] = Cell.SETTLED_WATER
+        for f in self.flowing_water:
+            self.grid[f.row][f.col] = Cell.FLOWING_WATER
+
+    def clear(self):
+        for f in self.springs:
+            self.grid[f.row][f.col] = Cell.SAND
+        for f in self.settled_water:
+            self.grid[f.row][f.col] = Cell.SAND
+        for f in self.flowing_water:
+            self.grid[f.row][f.col] = Cell.SAND
+
+    def is_clay(self, loc: Location) -> bool:
+        return self.grid[loc.row][loc.col] == Cell.CLAY
+
+    def is_clay(self, r: int, c: int) -> bool:
+        return self.grid[r][c] == Cell.CLAY
+
+    def is_settled_or_sand(self, loc: Location):
+        return self.grid[loc.row][loc.col] in [Cell.SAND, Cell.SETTLED_WATER]
+
+    def is_settled_or_sand(self, r: int, c: int):
+        return self.grid[r][c] in [Cell.SAND, Cell.SETTLED_WATER]
+
+    def flow(self, spring: Location):
+        """Let the water flow...
+        what are the steps in which water flows?
+        - we have to start with 1 spring of water (0,500)
+        - water flows down
+        - it will hit a bottom
+        - it fills from the bottom outwards till it hits a wall or can flow down again (if now wall)
+        - if a wall it fills the next row up again outwards till it hits a wall or can flow
+        - when it can flow again that will be counted as on or more new spring(s) for the next step
+        - this way we may be able to iterate flow steps?!
+        - as a step as described above would not change anymore it can be counted for all the watter (|,~)
+        - Stepping stops when the water goes above 0 or below max y
+        """
+
+        new_springs = set()
+        loc = spring
+        r = loc.row
+        c = loc.col
+        # walk down
+        while not self.is_clay(loc):
+            if loc.row >= self.min_rows:
+                self.flowing_water.add(loc)
+            if loc.row >= self.rows:
+                return new_springs
+            r += 1
+            loc = Location(r, c)
+        while True:
+            r -= 1
+            nloc = {Location(r, c)}
+            cleft = c
+            has_wall_left = False
+            while self.is_settled_or_sand(r, cleft):
+                cleft -= 1
+                ...
+
+    def flowing(self):
+        """While we have springs we need to keep flowing"""
+        while self.springs:
+            spring = self.springs.pop(0)
+            if spring in self.settled_springs:
+                continue
+            self.settled_springs.add(spring)
+            new_springs = self.flow(spring)
+        ...
 
     def __repr__(self) -> str:
         return "\n".join("".join(c for c in row) for row in self.grid)
@@ -81,8 +161,8 @@ class ReservoirResearch:
 
 def part_1(source):
     rr = ReservoirResearch(source)
-    _(rr)
-    return rr
+    # _(rr)
+    return rr.flowing()
 
 
 def part_2(source):
