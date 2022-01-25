ecb4316df0e9931a2b24b13c387ebabeaa72e966
Ivo Woltring <ivo@ivonet.nl>
2022-01-25 21:10:48 +0100

    2018 day 19 part 1 and slooooowwww part 2

--- 2018/day_19/day_19.py
+++ 2018/day_19/day_19.py
@@ -11,13 +11,14 @@ import os
 import sys
 import unittest
 from pathlib import Path
+from typing import NamedTuple
 
 from ivonet.files import read_rows
 from ivonet.iter import ints
 
 sys.dont_write_bytecode = True
 
-DEBUG = True
+DEBUG = False
 
 
 # noinspection DuplicatedCode
@@ -26,12 +27,116 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
+class Instruction(NamedTuple):
+    opcode: str
+    a: int
+    b: int
+    output: int  # register
+
+
+class ChronalClassification:
+
+    def __init__(self, source, instruction_pointer: int = 0, r0=0) -> None:
+        self.source = source
+        self.instructions: list[Instruction] = []
+        self.bound_reg = None
+        self.instruction_pointer: int = instruction_pointer
+        self.parse(self.source)
+        self.registers = {0: r0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+        self.all_ops = {
+            "addr": self.addr,
+            "addi": self.addi,
+            "mulr": self.mulr,
+            "muli": self.muli,
+            "banr": self.banr,
+            "bani": self.bani,
+            "borr": self.borr,
+            "bori": self.bori,
+            "setr": self.setr,
+            "seti": self.seti,
+            "gtir": self.gtir,
+            "gtri": self.gtri,
+            "gtrr": self.gtrr,
+            "eqir": self.eqir,
+            "eqri": self.eqri,
+            "eqrr": self.eqrr
+        }
+
+    def parse(self, source):
+        self.bound_reg = ints(source[0])[0]
+        opcodes = [line.split()[0] for line in source[1:]]
+        numbers = [ints(line) for line in source[1:]]
+        self.instructions = [Instruction(op, *nrs) for op, nrs in zip(opcodes, numbers)]
+
+    def background_process(self, reg=0):
+        while True:
+            try:
+                cmd = self.instructions[self.instruction_pointer]
+            except IndexError:
+                break
+            self.registers[self.bound_reg] = self.instruction_pointer
+            self.all_ops[cmd.opcode](cmd)
+            self.instruction_pointer = self.registers[self.bound_reg]
+            _(self.instruction_pointer, cmd
+              , self.registers)
+            self.instruction_pointer += 1
+        return self.registers[reg]
+
+    def addr(self, cmd: Instruction):
+        self.registers[cmd.output] = self.registers[cmd.a] + self.registers[cmd.b]
+
+    def addi(self, cmd: Instruction):
+        self.registers[cmd.output] = self.registers[cmd.a] + cmd.b
+
+    def mulr(self, cmd: Instruction):
+        self.registers[cmd.output] = self.registers[cmd.a] * self.registers[cmd.b]
+
+    def muli(self, cmd: Instruction):
+        self.registers[cmd.output] = self.registers[cmd.a] * cmd.b
+
+    def banr(self, cmd: Instruction):
+        self.registers[cmd.output] = self.registers[cmd.a] & self.registers[cmd.b]
+
+    def bani(self, cmd: Instruction):
+        self.registers[cmd.output] = self.registers[cmd.a] & cmd.b
+
+    def borr(self, cmd: Instruction):
+        self.registers[cmd.output] = self.registers[cmd.a] | self.registers[cmd.b]
+
+    def bori(self, cmd: Instruction):
+        self.registers[cmd.output] = self.registers[cmd.a] | cmd.b
+
+    def setr(self, cmd: Instruction):
+        self.registers[cmd.output] = self.registers[cmd.a]
+
+    def seti(self, cmd: Instruction):
+        self.registers[cmd.output] = cmd.a
+
+    def gtir(self, cmd: Instruction):
+        self.registers[cmd.output] = 1 if cmd.a > self.registers[cmd.b] else 0
+
+    def gtri(self, cmd: Instruction):
+        self.registers[cmd.output] = 1 if self.registers[cmd.a] > cmd.b else 0
+
+    def gtrr(self, cmd: Instruction):
+        self.registers[cmd.output] = 1 if self.registers[cmd.a] > self.registers[cmd.b] else 0
+
+    def eqir(self, cmd: Instruction):
+        self.registers[cmd.output] = 1 if cmd.a == self.registers[cmd.b] else 0
+
+    def eqri(self, cmd: Instruction):
+        self.registers[cmd.output] = 1 if self.registers[cmd.a] == cmd.b else 0
+
+    def eqrr(self, cmd: Instruction):
+        self.registers[cmd.output] = 1 if self.registers[cmd.a] == self.registers[cmd.b] else 0
+
+
 def part_1(source):
-    return None
+    return ChronalClassification(source).background_process()
 
 
 def part_2(source):
-    return None
+    return ChronalClassification(source, r0=1).background_process()
 
 
 class UnitTests(unittest.TestCase):
@@ -41,16 +146,20 @@ class UnitTests(unittest.TestCase):
             print()
         day = str(ints(Path(__file__).name)[0])
         self.source = read_rows(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
+        self.test_source = read_rows("""#ip 0
+seti 5 0 1
+seti 6 0 2
+addi 0 1 0
+addr 1 2 3
+setr 1 0 0
+seti 8 0 4
+seti 9 0 5""")
 
     def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+        self.assertEqual(6, part_1(self.test_source))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
-
-    def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(1482, part_1(self.source))
 
     def test_part_2(self):
         self.assertEqual(None, part_2(self.source))
