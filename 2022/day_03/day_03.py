53f9a7724fe8cd5532e86a867bb08f8c7f4dddae
Ivo Woltring <ivo@ivonet.nl>
2022-12-03 06:31:35 +0100

    day 3 part 2

--- 2022/day_03/day_03.py
+++ 2022/day_03/day_03.py
@@ -21,6 +21,7 @@ DEBUG = True
 
 alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+
 # noinspection DuplicatedCode
 def _(*args, end="\n"):
     if DEBUG:
@@ -39,7 +40,13 @@ def part_1(source):
 
 
 def part_2(source):
-    return None
+    groups = zip(*(iter(source),) * 3)
+    priority = 0
+    for group in groups:
+        common_chars = list(set(group[0]) & set(group[1]) & set(group[2]))
+        for char in common_chars:
+            priority += alphabet.index(char) + 1
+    return priority
 
 
 class UnitTests(unittest.TestCase):
@@ -63,10 +70,10 @@ CrZsJsPPZsGzwwsLwLmpwMDw""")
         self.assertEqual(8233, part_1(self.source))
 
     def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(70, part_2(self.test_source))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(2821, part_2(self.source))
 
 
 if __name__ == '__main__':
