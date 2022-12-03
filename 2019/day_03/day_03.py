eed2e4b2d1e16b9bcd16aedfece460973d598019
Ivo Woltring <ivo@ivonet.nl>
2022-12-03 18:07:35 +0100

    2019 day 3 prep

---
+++ 2019/day_03/day_03.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+#  -*- coding: utf-8 -*-
+from __future__ import annotations
+
+__author__ = "Ivo Woltring"
+__copyright__ = "Copyright (c) 2021 Ivo Woltring"
+__license__ = "Apache 2.0"
+__doc__ = """"""
+
+import os
+import sys
+import unittest
+from pathlib import Path
+
+from ivonet.files import read_rows
+from ivonet.iter import ints
+
+sys.dont_write_bytecode = True
+
+DEBUG = True
+
+
+# noinspection DuplicatedCode
+def _(*args, end="\n"):
+    if DEBUG:
+        print(" ".join(str(x) for x in args), end=end)
+
+
+def parse(value: str, delimiter: str = ",") -> list[list[str, int]]:
+    """Parse the input into a list of lists of strings and ints"""
+    return [[x[0], int(x[1:])] for x in [x for x in value.split(delimiter)]]
+
+
+def part_1(source):
+    return None
+
+
+def part_2(source):
+    return None
+
+
+class UnitTests(unittest.TestCase):
+
+    def setUp(self) -> None:
+        if DEBUG:
+            print()
+        day = str(ints(Path(__file__).name)[0])
+        self.source = read_rows(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")
+        self.test_source = read_rows("""R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83""")
+        self.test_source2 = read_rows("""R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7""")
+
+    def test_example_data_part_1(self):
+        self.assertEqual(159, part_1(self.test_source))
+        self.assertEqual(135, part_1(self.test_source2))
+
+    def test_part_1(self):
+        self.assertEqual(None, part_1(self.source))
+
+    def test_example_data_part_2(self):
+        self.assertEqual(None, part_2(self.test_source))
+
+    def test_part_2(self):
+        self.assertEqual(None, part_2(self.source))
+
+
+if __name__ == '__main__':
+    unittest.main()
