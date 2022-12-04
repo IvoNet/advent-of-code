12e20a0a79ff00f51e26a31dc1398b9aad70d7b9
Ivo Woltring <ivo@ivonet.nl>
2022-12-04 06:39:54 +0100

    2022 day 4 done

--- 2022/day_04/day_04.py
+++ 2022/day_04/day_04.py
@@ -13,7 +13,7 @@ import unittest
 from pathlib import Path
 
 from ivonet.files import read_rows
-from ivonet.iter import ints
+from ivonet.iter import ints, groupify, rangei
 
 sys.dont_write_bytecode = True
 
@@ -26,12 +26,45 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
+def expand_group(group):
+    ret = ""
+    for x in rangei(group[0], group[1]):
+        ret += str(x)
+    return ret
+
+
 def part_1(source):
-    return None
+    full_container = []
+    for x in source:
+        group1, group2 = groupify([abs(y) for y in ints(x)], 2)
+        start1, stop1 = group1
+        start2, stop2 = group2
+        if start1 >= start2 and stop1 <= stop2:
+            full_container.append(x)
+            continue
+        if start2 >= start1 and stop2 <= stop1:
+            full_container.append(x)
+            continue
+
+    _(full_container)
+    return len(full_container)
 
 
 def part_2(source):
-    return None
+    overlaps = []
+    for x in source:
+        group1, group2 = groupify([abs(y) for y in ints(x)], 2)
+        start1, stop1 = group1
+        start2, stop2 = group2
+        if start2 <= start1 <= stop2 or stop2 >= stop1 >= start2:
+            overlaps.append(x)
+            continue
+        if start1 <= start2 <= stop1 or stop1 >= stop2 >= start1:
+            overlaps.append(x)
+            continue
+
+    _(overlaps)
+    return len(overlaps)
 
 
 class UnitTests(unittest.TestCase):
@@ -41,19 +74,24 @@ class UnitTests(unittest.TestCase):
             print()
         day = str(ints(Path(__file__).name)[0])
         self.source = read_rows(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
+        self.test_source = read_rows("""2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8""")
 
     def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+        self.assertEqual(2, part_1(self.test_source))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
+        self.assertEqual(459, part_1(self.source))
 
     def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(4, part_2(self.test_source))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(779, part_2(self.source))
 
 
 if __name__ == '__main__':
