50c080abce777693ede7dfad19ae82858ed157d3
Ivo Woltring <ivo@ivonet.nl>
2022-12-05 07:32:03 +0100

    2022 day 5

--- 2022/day_05/day_05.py
+++ 2022/day_05/day_05.py
@@ -14,10 +14,12 @@ you can find that here: https://github.com/IvoNet/advent-of-code/tree/master/ivo
 import os
 import sys
 import unittest
+from collections import defaultdict
 from pathlib import Path
 
-from ivonet.files import read_rows
-from ivonet.iter import ints
+from ivonet.collection import Stack
+from ivonet.files import read_data, read_rows
+from ivonet.iter import ints, rangei
 
 sys.dont_write_bytecode = True
 
@@ -30,12 +32,60 @@ def _(*args, end="\n"):
         print(" ".join(str(x) for x in args), end=end)
 
 
+def parse_stacks(source):
+    rows = source.split("\n")
+    no_stacks = max(ints(rows[-1]))
+    stacks = defaultdict(Stack)
+    rows = rows[:-1][::-1]
+    for row in rows:
+        for s, i in enumerate(rangei(1, no_stacks * 4, 4)):
+            try:
+                if row[i] != " ":
+                    stacks[s + 1].push(row[i])
+            except IndexError:
+                _(i, "no more stacks")
+                break
+    return stacks
+
+
+def parse_moves(source):
+    return [ints(x) for x in read_rows(source)]
+
+
+def parse_input(source):
+    stacks, instructions = source.split("\n\n")
+    return parse_stacks(stacks), parse_moves(instructions)
+
+
 def part_1(source):
-    return None
+    stacks, moves = parse_input(source)
+
+    for times, from_, to_ in moves:
+        for _ in range(times):
+            stacks[to_].push(stacks[from_].pop())
+
+    ret = ""
+    for i in sorted(stacks.keys()):
+        ret += stacks[i].pop()
+
+    return ret
 
 
 def part_2(source):
-    return None
+    stacks, moves = parse_input(source)
+
+    for times, from_, to_ in moves:
+        temp = Stack()
+        for _ in range(times):
+            temp.push(stacks[from_].pop())
+        for _ in range(times):
+            stacks[to_].push(temp.pop())
+
+    ret = ""
+    for i in sorted(stacks.keys()):
+        ret += stacks[i].pop()
+
+    return ret
 
 
 class UnitTests(unittest.TestCase):
@@ -44,20 +94,28 @@ class UnitTests(unittest.TestCase):
         if DEBUG:
             print()
         day = str(ints(Path(__file__).name)[0])
-        self.source = read_rows(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input")
-        self.test_source = read_rows("""""")
+        self.source = read_data(f"{os.path.dirname(__file__)}/day_{day.zfill(2)}.input", raw=True)
+        self.test_source = read_data("""    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2""", raw=True)
 
     def test_example_data_part_1(self):
-        self.assertEqual(None, part_1(self.test_source))
+        self.assertEqual("CMZ", part_1(self.test_source))
 
     def test_part_1(self):
-        self.assertEqual(None, part_1(self.source))
+        self.assertEqual("LJSVLTWQM", part_1(self.source))
 
     def test_example_data_part_2(self):
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual("MCD", part_2(self.test_source))
 
     def test_part_2(self):
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual("BRQWDBBJM", part_2(self.source))
 
 
 if __name__ == '__main__':
