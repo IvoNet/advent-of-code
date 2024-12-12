579cd1be40d8b55b61c2433ba1c2a61f4bf14625
Ivo Woltring <ivo@ivonet.nl>
2024-12-12 21:02:34 +0100

    2024 day 06 - Part 1

--- y2024/day_06/day_06.py
+++ y2024/day_06/day_06.py
@@ -22,12 +22,38 @@ import sys
 from ivonet.decorators import debug
 from ivonet.decorators import timer
 from ivonet.files import read_rows
+from ivonet.grid import north, east, south, west, Location
 from ivonet.iter import ints
 
 collections.Callable = collections.abc.Callable  # type: ignore
 sys.dont_write_bytecode = True
 
 DEBUG = True
+
+OBSTACLE = "#"
+FREE = "."
+STARTERS = "^>v<"
+WALKED = "X"
+START = {
+    "^": "N",
+    ">": "E",
+    "v": "S",
+    "<": "W",
+}
+
+NEXT_DIRECTION = {
+    "N": "E",
+    "E": "S",
+    "S": "W",
+    "W": "N",
+}
+
+DIRECTION_FUNCTION = {
+    "N": north,
+    "E": east,
+    "S": south,
+    "W": west,
+}
 
 
 # noinspection DuplicatedCode
@@ -36,10 +62,51 @@ def p(*args, end="\n", sep=" "):
         print(sep.join(str(x) for x in args), end=end)
 
 
+def direction(start):
+    while True:
+        yield start
+        start = NEXT_DIRECTION[start]
+
+
+
+
 @debug
 @timer
 def part_1(source) -> int | None:
+    grid = [list(row) for row in source]
     answer = 0
+    loc: Location
+    direction_gen: str
+    for h, row in enumerate(source):
+        for w, col in enumerate(row):
+            if col in STARTERS:
+                loc = Location(h, w)
+                grid[h][w] = WALKED
+                direction_gen = START[col]
+                break
+
+    for d in direction(direction_gen):
+        to = DIRECTION_FUNCTION[d]
+        loc, value = next(to(grid, loc, value=True))
+        while value != OBSTACLE:
+            if value == FREE:
+                answer += 1
+                grid[loc[0]][loc[1]] = WALKED
+            last_loc = loc
+            loc, value = next(to(grid, loc, value=True), (None, None))
+            if value == OBSTACLE:
+                loc = last_loc
+            if not loc and not value:
+                for row in grid:
+                    print("".join(row))
+
+                return sum([row.count(WALKED) for row in grid])
+
+
+
+
+
+
     pyperclip.copy(str(answer))
     return answer
 
@@ -56,10 +123,10 @@ def part_2(source) -> int | None:
 class UnitTests(unittest.TestCase):
 
     def test_example_data_part_1(self) -> None:
-        self.assertEqual(None, part_1(self.test_source))
+        self.assertEqual(41, part_1(self.test_source))
 
     def test_part_1(self) -> None:
-        self.assertEqual(None, part_1(self.source))
+        self.assertEqual(5153, part_1(self.source))
 
     def test_example_data_part_2(self) -> None:
         self.assertEqual(None, part_2(self.test_source))
