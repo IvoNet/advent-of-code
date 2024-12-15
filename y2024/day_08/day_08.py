5a96ca7a5c7e9c698aa2d4fada8517e3d8f624b4
Ivo Woltring <ivo@ivonet.nl>
2024-12-15 21:37:26 +0100

    update part 2s

--- y2024/day_08/day_08.py
+++ y2024/day_08/day_08.py
@@ -15,6 +15,7 @@ import collections
 import os
 import sys
 import unittest
+from collections import abc, defaultdict
 from pathlib import Path
 
 import pyperclip
@@ -22,12 +23,13 @@ import pyperclip
 from ivonet.decorators import debug
 from ivonet.decorators import timer
 from ivonet.files import read_rows
+from ivonet.grid import Location
 from ivonet.iter import ints
 
-collections.Callable = collections.abc.Callable  # type: ignore
+collections.Callable = abc.Callable  # type: ignore
 sys.dont_write_bytecode = True
 
-DEBUG = True
+DEBUG = False
 
 
 # noinspection DuplicatedCode
@@ -36,18 +38,65 @@ def p(*args, end="\n", sep=" "):
         print(sep.join(str(x) for x in args), end=end)
 
 
+def parse(source):
+    antennas = defaultdict(list)
+    grid = []
+    for r, line in enumerate(source):
+        grid.append(list(line))
+        for c, ch in enumerate(line):
+            if ch != '.':
+                antennas[ch].append(Location(r, c))
+
+    return grid, antennas, len(grid), len(grid[0])
+
+
 @debug
 @timer
 def part_1(source) -> int | None:
-    answer = 0
+    answer = antinode_processor(source)
     pyperclip.copy(str(answer))
     return answer
+
+
+def antinode_processor(source, part2=False):
+    answer = set()
+    grid, antennas, height, width = parse(source)
+    for r in range(height):
+        for c in range(width):
+            for _, values in antennas.items():  # _ = key unused, vs = values
+                for (r1, c1) in values:
+                    for (r2, c2) in values:
+                        if (r1, c1) != (r2, c2):  # not the same antenna
+                            d1 = abs(r - r1) + abs(c - c1)
+                            d2 = abs(r - r2) + abs(c - c2)
+
+                            dr1 = r - r1  # delta row
+                            dc1 = c - c1  # delta column
+
+                            dr2 = r - r2  # delta row
+                            dc2 = c - c2  # delta column
+                            if part2:
+                                if 0 <= r < height:
+                                    if 0 <= c < width:
+                                        if dr1 * dc2 == dr2 * dc1:
+                                            answer.add((r, c))
+                            else:
+                                if d1 == 2 * d2 or d1 * 2 == d2:  # must be twice the distance
+                                    if 0 <= r < height:  # not above or below the grid
+                                        if 0 <= c < width:  # not left or right of the grid
+                                            if dr1 * dc2 == dr2 * dc1:
+                                                answer.add((r, c))
+    if DEBUG:
+        for r, c in answer:
+            grid[r][c] = '#'
+        print("\n".join("".join(row) for row in grid))
+    return len(answer)
 
 
 @debug
 @timer
 def part_2(source) -> int | None:
-    answer = 0
+    answer = antinode_processor(source, True)
     pyperclip.copy(str(answer))
     return answer
 
@@ -59,13 +108,13 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(14, part_1(self.test_source))
 
     def test_part_1(self) -> None:
-        self.assertEqual(None, part_1(self.source))
+        self.assertEqual(426, part_1(self.source))
 
     def test_example_data_part_2(self) -> None:
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(34, part_2(self.test_source))
 
     def test_part_2(self) -> None:
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(1359, part_2(self.source))
 
     def setUp(self) -> None:
         print()
