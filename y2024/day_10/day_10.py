65bdc426bac94cba354b733d34f2a9c30523d772
Ivo Woltring <ivo@ivonet.nl>
2024-12-16 21:19:59 +0100

    2024 day 16

--- y2024/day_10/day_10.py
+++ y2024/day_10/day_10.py
@@ -15,8 +15,8 @@ import collections
 import os
 import sys
 import unittest
+from collections import abc, defaultdict
 from pathlib import Path
-from typing import Callable, Optional
 
 import pyperclip
 
@@ -26,9 +26,9 @@ from ivonet.decorators import timer
 from ivonet.files import read_rows
 from ivonet.grid import Location
 from ivonet.iter import ints
-from ivonet.search import bfs, T, Node
+from ivonet.search import T, Node
 
-collections.Callable = collections.abc.Callable  # type: ignore
+collections.Callable = abc.Callable  # type: ignore
 sys.dont_write_bytecode = True
 
 DEBUG = True
@@ -49,24 +49,31 @@ class Trails:
         self.width: int = len(source[0])
         self.starts: list[Location] = []
         self.goals: list[Location] = []
+        self.distinct_paths = defaultdict(int)
         for r, line in enumerate(source):
             row = []
             for c, ch in enumerate(line):
-                if ch == "0":
+                if ch == "0":  # Starting locations
                     self.starts.append(Location(r, c))
-                if ch == "9":
+                if ch == "9":  # Goal locations
                     self.goals.append(Location(r, c))
                 row.append(int(ch))
             self.grid.append(row)
 
     def is_goal(self, loc: Location) -> bool:
+        """Check if the location is a goal"""
         return loc in self.goals
 
     def successors(self, loc: Location) -> list[Location]:
+        """Return a list of locations that can be reached from the current location
+        Note that only directions left, right, up and down are allowed and only if
+        the next value is the current value + 1 and no more.
+        """
         next_value = self.grid[loc.row][loc.col] + 1
         if next_value > 9:
             return []
         ret: list[Location] = []
+
         left = Location(loc.row, loc.col - 1)
         if left.col >= 0 and self.grid[left.row][left.col] == next_value:
             ret.append(left)
@@ -81,40 +88,69 @@ class Trails:
             ret.append(down)
         return ret
 
+    def bfs_part1(self, initial: T, ) -> int:
+        """Breadth First Search for part 1
 
-def bfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], list[T]]) -> Optional[Node[T]]:
-    # frontier is where we've yet to go
-    frontier: Queue[Node[T]] = Queue()
-    frontier.push(Node(initial, None))
-    # explored is where we've been
-    explored: set[T] = {initial}
-    answer = 0
-    # keep going while there is more to explore
-    while not frontier.empty:
-        current_node: Node[T] = frontier.pop()
-        current_state: T = current_node.state
-        # if we found the goal, we're done
-        if goal_test(current_state):
-            answer += 1
-        # check where we can go next and haven't explored
-        for child in successors(current_state):
-            if child in explored:  # skip children we already explored
-                continue
-            explored.add(child)
-            frontier.push(Node(child, current_node))
-    return answer
+        Kinda standard BFS but with a twist.
+        We need to count the number of possible paths to the goal
+        """
+        # frontier is where we've yet to go
+        frontier: Queue[Node[T]] = Queue()
+        frontier.push(Node(initial, None))
+        # explored is where we've been
+        explored: set[T] = {initial}
+        answer = 0
+        # keep going while there is more to explore
+        while not frontier.empty:
+            current_node: Node[T] = frontier.pop()
+            current_state: T = current_node.state
+            # if we found the goal, we're done
+            if self.is_goal(current_state):
+                answer += 1
+            # check where we can go next and haven't explored
+            for child in self.successors(current_state):
+                if child in explored:  # skip children we already explored
+                    continue
+                explored.add(child)
+                frontier.push(Node(child, current_node))
+        return answer
+
+    def bfs_part2(self, initial: T) -> int:
+        """Breadth First Search for part 2
+        This adjusted version of a BFS is used to count the number of distinct paths to the goal
+        """
+        if self.grid[initial.row][initial.col] == 9:
+            return 1
+        if initial in self.distinct_paths:
+            return self.distinct_paths[initial]
+        answer = 0
+        for child in self.successors(initial):
+            answer += self.bfs_part2(child)
+        self.distinct_paths[initial] = answer
+        return answer
+
+    def part_1(self):
+        answer = 0
+        for start in self.starts:
+            solution = self.bfs_part1(start)
+            if solution:
+                answer += solution
+        return answer
+
+    def part_2(self):
+        answer = 0
+        for start in self.starts:
+            solution = self.bfs_part2(start)
+            if solution:
+                answer += solution
+        return answer
 
 
 @debug
 @timer
 def part_1(source) -> int | None:
-    answer = 0
     trails = Trails(source)
-    for start in trails.starts:
-        solution = bfs(start, trails.is_goal, trails.successors)
-        if solution:
-            answer += solution
-
+    answer = trails.part_1()
     pyperclip.copy(str(answer))
     return answer
 
@@ -122,7 +158,8 @@ def part_1(source) -> int | None:
 @debug
 @timer
 def part_2(source) -> int | None:
-    answer = 0
+    trails = Trails(source)
+    answer = trails.part_2()
     pyperclip.copy(str(answer))
     return answer
 
@@ -137,10 +174,10 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(582, part_1(self.source))
 
     def test_example_data_part_2(self) -> None:
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(81, part_2(self.test_source))
 
     def test_part_2(self) -> None:
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(1302, part_2(self.source))
 
     def setUp(self) -> None:
         print()
