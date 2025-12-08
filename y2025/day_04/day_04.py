96abad104d11632c3820628e8fee7689f20ff680
Ivo Woltring <ivo@ivonet.nl>
2025-12-08 17:04:39 +0100

    2025 day 4

--- y2025/day_04/day_04.py
+++ y2025/day_04/day_04.py
@@ -20,6 +20,7 @@ import sys
 from ivonet.decorators import debug
 from ivonet.decorators import timer
 from ivonet.files import read_rows
+from ivonet.grid import Location, neighbor_values
 from ivonet.iter import ints
 
 collections.Callable = collections.abc.Callable  # type: ignore
@@ -34,10 +35,36 @@ def p(*args, end="\n", sep=" "):
         print(sep.join(str(x) for x in args), end=end)
 
 
+def get_removable_rolls(grid) -> list[Location]:
+    ret = []
+    for x, row in enumerate(grid):
+        for y, col in enumerate(row):
+            if col == "@":
+                values = neighbor_values(grid, Location(x, y))
+                count = values.count("@")
+                if count < 4:
+                    ret.append((x, y))
+    return ret
+
+
+def process_rolls_of_paper(source):
+    grid = [list(row) for row in source]
+    count = 0
+    while True:
+        removable = get_removable_rolls(grid)
+        if not removable:
+            break
+        count += len(removable)
+        for x, y in removable:
+            grid[x][y] = "."
+    return count
+
+
 @debug
 @timer
 def part_1(source) -> int | None:
-    answer = 0
+    grid = [list(row) for row in source]
+    answer = len(get_removable_rolls( grid))
     pyperclip.copy(str(answer))
     return answer
 
@@ -45,7 +72,7 @@ def part_1(source) -> int | None:
 @debug
 @timer
 def part_2(source) -> int | None:
-    answer = 0
+    answer = process_rolls_of_paper( source)
     pyperclip.copy(str(answer))
     return answer
 
@@ -54,16 +81,16 @@ def part_2(source) -> int | None:
 class UnitTests(unittest.TestCase):
 
     def test_example_data_part_1(self) -> None:
-        self.assertEqual(None, part_1(self.test_source))
+        self.assertEqual(13, part_1(self.test_source))
 
     def test_part_1(self) -> None:
-        self.assertEqual(None, part_1(self.source))
+        self.assertEqual(1395, part_1(self.source))
 
     def test_example_data_part_2(self) -> None:
-        self.assertEqual(None, part_2(self.test_source))
+        self.assertEqual(43, part_2(self.test_source))
 
     def test_part_2(self) -> None:
-        self.assertEqual(None, part_2(self.source))
+        self.assertEqual(8451, part_2(self.source))
 
     def setUp(self) -> None:
         print()
