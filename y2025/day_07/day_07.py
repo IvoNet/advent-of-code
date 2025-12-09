ca01ac30b62afd03d0caa26dbe98b8ba40fbd1bc
Ivo Woltring <ivo@ivonet.nl>
2025-12-09 13:46:12 +0100

    2025 day 7 start

---
+++ y2025/day_07/day_07.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+#  -*- coding: utf-8 -*-
+from __future__ import annotations
+
+__author__ = "Ivo Woltring"
+__copyright__ = "Copyright (c) 2023 Ivo Woltring"
+__license__ = "Apache 2.0"
+__doc__ = """
+If a part of the code is not part of the post it is probably 
+code I found reusable and I may have moved it to my 'ivonet' library.
+you can find that here: https://github.com/IvoNet/advent-of-code/tree/master/ivonet
+"""
+
+import collections
+import unittest
+from pathlib import Path
+
+import pyperclip
+import sys
+from ivonet.decorators import debug
+from ivonet.decorators import timer
+from ivonet.files import read_rows
+from ivonet.iter import ints
+
+collections.Callable = collections.abc.Callable  # type: ignore
+sys.dont_write_bytecode = True
+
+DEBUG = True
+
+
+# noinspection DuplicatedCode
+def p(*args, end="\n", sep=" "):
+    if DEBUG:
+        print(sep.join(str(x) for x in args), end=end)
+
+
+@debug
+@timer
+def part_1(source) -> int | None:
+    answer = 0
+    pyperclip.copy(str(answer))
+    return answer
+
+
+@debug
+@timer
+def part_2(source) -> int | None:
+    answer = 0
+    pyperclip.copy(str(answer))
+    return answer
+
+
+# noinspection DuplicatedCode
+class UnitTests(unittest.TestCase):
+
+    def test_example_data_part_1(self) -> None:
+        self.assertEqual(21, part_1(self.test_source))
+
+    def test_part_1(self) -> None:
+        self.assertEqual(None, part_1(self.source))
+
+    def test_example_data_part_2(self) -> None:
+        self.assertEqual(None, part_2(self.test_source))
+
+    def test_part_2(self) -> None:
+        self.assertEqual(None, part_2(self.source))
+
+    def setUp(self) -> None:
+        print()
+        folder = Path(__file__).resolve().parent
+        day = f"{ints(Path(__file__).stem)[0]:02}"
+        self.source = read_rows(f"{folder}/day_{day}.input")
+        self.test_source = read_rows(f"{folder}/test_{day}.input")
+
+
+if __name__ == '__main__':
+    unittest.main()
